ac0cbf694952a0470460a495a4be8441274aaa5c
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2024-12-22 17:52:09 -0500

    Update dl-play.js

--- plugins/dl-play.js
+++ plugins/dl-play.js
@@ -4,7 +4,7 @@ import yts from "yt-search";
 
 let handler = async (m, { conn, text }) => {
     if (!text) {
-        return conn.reply(m.chat, "❀ Ingresa un enlace de YouTube válido.", m,rcanal);
+        return conn.reply(m.chat, "❀ Ingresa texto \nejenplo: .play ozuna", m,rcanal);
     }
 
     await m.react("🕓");
