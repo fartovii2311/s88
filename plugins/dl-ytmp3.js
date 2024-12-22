6ff95ea41a96d214e6a736073d98e8d6d7fa8d53
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2024-12-22 16:34:17 -0500

    Update dl-ytmp3.js

--- plugins/dl-ytmp3.js
+++ plugins/dl-ytmp3.js
@@ -1,18 +1,31 @@
 // *[ ❀ YTMP3 ]*
-import fetch from 'node-fetch'
+import fetch from 'node-fetch';
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) return conn.reply(m.chat, `❀ Ingresa un  link de youtube`, m)
-    
-try {
-let api = await (await fetch(`https://api.siputzx.my.id/api/d/ytmp3?url=${text}`)).json()
-let dl_url = api.data.dl
+    if (!text) return conn.reply(m.chat, `❀ Ingresa un link de YouTube`, m);
 
-conn.sendMessage(m.chat, { audio: { url: dl_url }, mimetype: "audio/mp4", ptt: true }, { quoted: m })
-} catch (error) {
-console.error(error)
-}}
+    try {
+        let apiResponse = await fetch(`https://api.zenkey.my.id/api/download/ytmp3?apikey=zenkey&url=${text}`);
+        let api = await apiResponse.json();
 
-handler.command = ['ytmp3']
+        if (api.status === true) {
+            let dl_url = api.result.download.url;
 
-export default handler
+            conn.sendMessage(m.chat, { 
+                audio: { url: dl_url },
+                mimetype: "audio/mp3",
+                ptt: true
+            }, { quoted: m });
+
+        } else {
+            conn.reply(m.chat, '❀ Hubo un error al obtener el enlace de descarga. Intenta nuevamente.', m);
+        }
+    } catch (error) {
+        console.error('Error al obtener el MP3:', error);
+        conn.reply(m.chat, '❀ Ocurrió un error al intentar descargar el MP3. Intenta nuevamente más tarde.', m);
+    }
+}
+
+handler.command = ['ytmp3'];
+
+export default handler;
