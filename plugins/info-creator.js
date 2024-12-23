05f7ca5a5a15823d52fe520062aa27e173b2ac3e
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2024-12-23 11:22:34 -0500

    Update info-creator.js

--- plugins/info-creator.js
+++ plugins/info-creator.js
@@ -1,21 +1,51 @@
-import fetch from 'node-fetch'
+import fetch from 'node-fetch';
+
 let handler = async (m, { conn, usedPrefix, text, args, command }) => {
-m.react('🤍')
+    m.react('🤍');
 
-let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
+    let fkontak = { 
+        "key": { 
+            "participants": "0@s.whatsapp.net", 
+            "remoteJid": "status@broadcast", 
+            "fromMe": false, 
+            "id": "Halo" 
+        }, 
+        "message": { 
+            "contactMessage": { 
+                "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` 
+            }
+        }, 
+        "participant": "0@s.whatsapp.net" 
+    };
 
-let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-let name = await conn.getName(who)
-let edtr = `@${m.sender.split`@`[0]}`
+    let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
+    let name = await conn.getName(who);
+    let edtr = `@${m.sender.split`@`[0]}`;
 
-let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:WhatsApp;   🍃\nNICKNAME:\nORG:  \nTITLE:\nitem1.TEL;waid=:+\nitem1.X-ABLabel:📞 WhatsApp Owner\nitem2.URL:https://github.com/Angelito-OFC\nitem2.X-ABLabel:💬 More\nitem3.EMAIL;type=INTERNET: agasistencia2@gmail.com\nitem3.X-ABLabel:💌 Correo soporte\nitem4.ADR:;;🇦🇷 Argentina;;;;\nitem4.X-ABADR:💬 More\nitem4.X-ABLabel: Localización 🫧\nBDAY;value=date:🤍 09-12-2007\nEND:VCARD`
-const tag_own = await conn.sendMessage(m.chat, { contacts: { displayName: packname, contacts: [{ vcard }] }}, { quoted: fkontak })
-let caption = `*👋 Hola ${edtr}, este es el contacto de mi creador 🤍*`
-    await conn.reply(m.chat, caption, tag_own, { mentions: conn.parseMention(caption) })
+    let vcard = `BEGIN:VCARD
+VERSION:3.0
+N:WhatsApp;   🍃
+FN:Owner Contact
+ORG:Owner
+TEL;TYPE=cell:+51968382008
+EMAIL:agasistencia2@gmail.com
+ADR:;;🇦🇷 Argentina;;;;;;
+BDAY;value=date:🤍 09-12-2007
+END:VCARD`;
 
-}
-handler.help = ['owner', 'creator']
-handler.tags = ['main']
-handler.command = /^(owner|creator|creador|dueño)$/i
+    const tag_own = await conn.sendMessage(m.chat, { 
+        contacts: { 
+            displayName: 'Owner Contact', 
+            contacts: [{ vcard }] 
+        } 
+    }, { quoted: fkontak });
 
-export default handler
+    let caption = `*👋 Hola ${edtr}, este es el contacto de mi creador 🤍*`;
+    await conn.reply(m.chat, caption, tag_own, { mentions: conn.parseMention(caption) });
+};
+
+handler.help = ['owner', 'creator'];
+handler.tags = ['main'];
+handler.command = /^(owner|creator|creador|dueño)$/i;
+
+export default handler;
