0e05b2995be565a56364f5f832b195a610480dac
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2024-12-23 20:16:54 -0500

    Update config.js

--- config.js
+++ config.js
@@ -151,6 +151,21 @@ global.APIKeys = {
 'https://api-xcoders.site': 'Frieren'
 }
 
+// ❰❰ API KEYS ❱❱
+global.Key360 = ["Gata_Dios"] // key Ephoto360
+global.openai_key = 'sk-0' // Api New: https://platform.openai.com/account/api-keys 
+global.openai_org_id = 'org-3' // Api New: https://platform.openai.com/account/org-settings */
+global.keysZens = ["LuOlangNgentot", "c2459db922", "37CC845916", "6fb0eff124", "hdiiofficial", "fiktod", "BF39D349845E", "675e34de8a", "0b917b905e6f"]
+global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
+global.keysxteammm = ["29d4b59a4aa687ca", "5LTV57azwaid7dXfz5fzJu", "cb15ed422c71a2fb", "5bd33b276d41d6b4", "HIRO", "kurrxd09", "ebb6251cc00f9c63"]
+global.keysxteam = keysxteammm[Math.floor(keysxteammm.length * Math.random())]
+global.keysneoxrrr = ["5VC9rvNx", "cfALv5"]
+global.keysneoxr = keysneoxrrr[Math.floor(keysneoxrrr.length * Math.random())]
+global.lolkeysapi = "GataDiosV3"
+global.itsrose = ["4b146102c4d500809da9d1ff"]
+global.baileys = "@whiskeysockets/baileys"
+global.apis = 'https://deliriussapi-oficial.vercel.app'
+
 let file = fileURLToPath(import.meta.url)
 watchFile(file, () => {
   unwatchFile(file)
