43acc8375556f3dbe5dc3c4ff0ff0b2d7f273c44
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2024-12-26 13:17:51 -0500

    Update rpg-shop.js

--- plugins/rpg-shop.js
+++ plugins/rpg-shop.js
@@ -1,7 +1,8 @@
 const xppercorazones = 450
+const xpperlimit = 2
 let handler = async (m, { conn, command, args }) => {
   let count = command.replace(/^buycoins/i, '')
-  count = count ? /all/i.test(count) ? Math.floor(global.db.data.users[m.sender].exp) : parseInt(count) : args[0] ? parseInt(args[0]) : 1
+  count = count ? /all/i.test(count) ? Math.floor(global.db.data.users[m.sender].exp / xpperlimit) : parseInt(count) : args[0] ? parseInt(args[0]) : 1
   count = Math.max(1, count)
   if (global.db.data.users[m.sender].exp >= xpperlimit * count) {
     global.db.data.users[m.sender].exp -= xpperlimit * count
