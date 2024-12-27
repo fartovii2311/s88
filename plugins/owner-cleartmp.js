937379c7548f8b4d32bed828b1963f682102e7b6
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2024-12-27 08:49:08 -0500

    Update owner-cleartmp.js

--- plugins/owner-cleartmp.js
+++ plugins/owner-cleartmp.js
@@ -1,51 +1,37 @@
-import { tmpdir } from 'os'
-import path, { join } from 'path'
-import { readdirSync, statSync, unlinkSync, existsSync } from 'fs'
+import { join } from 'path'
+import { readdirSync, unlinkSync, existsSync } from 'fs'
 
-let handler = async (m, { conn, usedPrefix: _p, __dirname, args }) => {
-
-  const tmpDirs = [tmpdir(), join(__dirname, '../tmp')];
-  const filenames = [];
-
-  // Leer archivos de los directorios si existen
-  tmpDirs.forEach(dirname => {
-    if (existsSync(dirname)) {
-      readdirSync(dirname).forEach(file => {
-        filenames.push(join(dirname, file));
-      });
-    }
-  });
-
+let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
+  const tmpDir = join(__dirname, '../tmp');
   const deletedFiles = [];
 
-  // Eliminar archivos
-  filenames.forEach(file => {
-    const stats = statSync(file);
+  if (existsSync(tmpDir)) {
+    const filenames = readdirSync(tmpDir);
 
-    if (stats.isDirectory()) {
-      console.log(`Skipping directory: ${file}`);
-    } else {
+    filenames.forEach(file => {
+      const filePath = join(tmpDir, file);
+
       try {
-        unlinkSync(file);  // Intentar eliminar el archivo
+        unlinkSync(filePath);
         deletedFiles.push(file);
       } catch (error) {
-        console.error(`Error al eliminar el archivo: ${file}`, error);
+        console.error(`Error al eliminar el archivo: ${filePath}`, error);
       }
-    }
-  });
+    });
 
-  // Responder al usuario
-  if (deletedFiles.length > 0) {
-    console.log('Deleted files:', deletedFiles);
-    const deletedFilesList = deletedFiles.join('\n');
-    // Si hay muchos archivos eliminados, solo mostrar los primeros 10
-    const responseMessage = deletedFiles.length > 10 ? 
-      `Archivos eliminados (mostrando los primeros 10):\n${deletedFilesList.slice(0, 300)}` :
-      `Archivos eliminados:\n${deletedFilesList}`;
-    
-    conn.reply(m.chat, responseMessage, m);
+    if (deletedFiles.length > 0) {
+      console.log('Archivos eliminados:', deletedFiles);
+      const deletedFilesList = deletedFiles.join('\n');
+      const responseMessage = deletedFiles.length > 10 ? 
+        `Archivos eliminados (mostrando los primeros 10):\n${deletedFilesList.slice(0, 300)}` :
+        `Archivos eliminados:\n${deletedFilesList}`;
+      
+      conn.reply(m.chat, responseMessage, m);
+    } else {
+      conn.reply(m.chat, 'No hay archivos para limpiar en tmp', m);
+    }
   } else {
-    conn.reply(m.chat, 'No hay archivos para limpiar en tmp', m);
+    conn.reply(m.chat, 'La carpeta tmp no existe', m);
   }
 
   conn.reply(m.chat, '✧ Listo!', m);
