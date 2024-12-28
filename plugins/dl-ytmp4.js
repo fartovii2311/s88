dcc25eb5b6fae557890e20cc3a67e0ace0d7143d
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2024-12-27 23:03:32 -0500

    Update dl-ytmp4.js

--- plugins/dl-ytmp4.js
+++ plugins/dl-ytmp4.js
@@ -1,25 +1,55 @@
-// *[ ❀ YTMP4 ]*
+// [ ❀ YTMP4 ]
 import fetch from 'node-fetch';
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-  if (!text) return conn.reply(m.chat, `❀ Ingresa un link de YouTube válido`, m,rcanal);
+  let [url, resolution] = text.split(' ');
+  if (!url) {
+    return conn.reply(m.chat, `Ingresa el link de un video de YouTube y una calidad. Ejemplo: ${usedPrefix + command} link 360`, m);
+  }
 
-  await m.react('🕓');
+  await m.react('🕓'); // Reacción de carga
 
   try {
-    let api = await (await fetch(`https://api.siputzx.my.id/api/d/ytmp4?url=${text}`)).json();
-    let dl_url = api.data.dl;
-    await conn.sendMessage(m.chat, { video: { url: dl_url }, caption: `*Aqui tiene ฅ^•ﻌ•^ฅ*` },{ quoted: m });
+    let apiinfo = await fetch(`https://ytdownloader.nvlgroup.my.id/info?url=${url}`);
+    let jsoninfo = await apiinfo.json();
+    let titulo = jsoninfo.title;
+    let duracion = jsoninfo.duration || '-';
+    let calidad = resolution || '360';
+    let img = jsoninfo.thumbnail;
+    let dl_url = `https://ytdownloader.nvlgroup.my.id/download?url=${url}&resolution=${calidad}`;
+    let vidFetch = await fetch(dl_url);
+    let video = await vidFetch.buffer();
+    let Tamaño = video.length / (1024 * 1024); // Tamaño en MB
 
-    await m.react('✅');
+    let HS = `- Titulo: ${titulo}
+- Link: ${url}
+- Duración: ${duracion}
+- Calidad: ${calidad}`;
+
+    if (Tamaño > 100) {
+      await conn.sendMessage(m.chat, { 
+        document: video, 
+        caption: HS, 
+        mimetype: 'video/mp4', 
+        fileName: `${titulo}.mp4` 
+      });
+    } else {
+      await conn.sendMessage(m.chat, { 
+        video: video, 
+        caption: HS, 
+        mimetype: 'video/mp4' 
+      });
+    }
+
+    await m.react('✅'); // Reacción de éxito
+
   } catch (error) {
-    await m.react('❌');
+    console.error('Error al descargar el video:', error);
+    m.reply('❀ Ocurrió un error al intentar obtener el video. Intenta nuevamente.');
+    await m.react('❌'); // Reacción de error
   }
 };
 
-handler.help = ['ytmp4 *<url>*'];
-handler.tags = ['downloader'];
 handler.command = ['ytmp4'];
-handler.register = true;
 
 export default handler;
