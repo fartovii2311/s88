bf08ae611f8600ff379760a9c3749babb5623cc1
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2024-12-27 22:40:43 -0500

    Update dl-ytmp3.js

--- plugins/dl-ytmp3.js
+++ plugins/dl-ytmp3.js
@@ -1,20 +1,26 @@
 // [ ❀ YTMP3 ]
-import fetch from 'node-fetch'
+import fetch from 'node-fetch';
 
-let HS = async (m, { conn, text }) => {
-if (!text) {
-return m.reply("❀ Ingresa un link de youtube")
-}
-    
-try {
-let api = await fetch(https://api.giftedtech.my.id/api/download/dlmp3?apikey=gifted&url=${text})
-let json = await api.json()
-let { quality, title, download_url } = json.result
+let handler = async (m, { conn, text }) => {
+  if (!text) {
+    return m.reply("❀ Ingresa un link de YouTube");
+  }
 
-await conn.sendMessage(m.chat, { audio: { url: download_url }, fileName: ${title}.mp3, mimetype: 'audio/mp4' }, { quoted: m })
-} catch (error) {
-console.error(error)
-}}
+  try {
+    let api = await fetch(`https://api.giftedtech.my.id/api/download/dlmp3?apikey=gifted&url=${text}`);
+    let json = await api.json();
+    let { quality, title, download_url } = json.result;
+
+    await conn.sendMessage(m.chat, { 
+      audio: { url: download_url }, 
+      fileName: `${title}.mp3`, 
+      mimetype: 'audio/mp4' 
+    }, { quoted: m });
+  } catch (error) {
+    console.error('Error al obtener el MP3:', error);
+    m.reply('❀ Ocurrió un error al intentar obtener el MP3. Intenta nuevamente.');
+  }
+};
 
 handler.command = ['ytmp3'];
 handler.register = true;
