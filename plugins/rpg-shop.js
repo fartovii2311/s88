815a773ec32dda86603bc622333acbf3841f6177
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2024-12-27 21:04:05 -0500

    Update rpg-shop.js

--- plugins/rpg-shop.js
+++ plugins/rpg-shop.js
@@ -1,5 +1,5 @@
 const xppercorazones = 450
-const xpperlimit = 2
+const xpperlimit = 450
 let handler = async (m, { conn, command, args }) => {
   let count = command.replace(/^buycoins/i, '')
   count = count ? /all/i.test(count) ? Math.floor(global.db.data.users[m.sender].exp / xpperlimit) : parseInt(count) : args[0] ? parseInt(args[0]) : 1
