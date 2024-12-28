28beec48a9a1a7c314dbd44d10bf69e3712f613c
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2024-12-27 21:48:37 -0500

    Update serbot-bots.js

--- plugins/serbot-bots.js
+++ plugins/serbot-bots.js
@@ -1,4 +1,4 @@
-import fetch from 'node-fetch'
+import fs from 'fs'
 import ws from 'ws'
 
 async function handler(m, { conn: stars, usedPrefix }) {
@@ -17,26 +17,18 @@ async function handler(m, { conn: stars, usedPrefix }) {
   let users = [...uniqueUsers.values()]
   let totalUsers = uniqueUsers.size
 
-  // Usar node-fetch con timeout
-  let img
-  try {
-    img = await fetch('https://pomf2.lain.la/f/hg3otwi4.jpg', {
-      timeout: 5000, // Tiempo de espera de 5 segundos
-    }).then(res => res.buffer())
-  } catch (err) {
-    console.error('Error al obtener la imagen:', err)
-    img = null
-  }
+  let img = fs.readFileSync('../storage/img/avatar_contact.png')
 
   let message = users.map((v, index) => `
-*[ \`${index + 1}\` - ${v.user.name || 'Sin Nombre'} ]*\n🤍 *Link:* https://wa.me/${v.user.jid.replace(/[^0-9]/g, '')}
+*[ \`${index + 1}\` - ${v.user.name || 'Sin Nombre'} ]*
+🤍 *Link:* https://wa.me/${v.user.jid.replace(/[^0-9]/g, '')}
 `).join('\n\n')
 
   let responseMessage = `🟢 *Subbots Activos: ${totalUsers}*\n\n${message.trim() || '_No hay subbots activos en este momento._'}`
 
   await stars.sendFile(
     m.chat,
-    img || 'default-image.jpg',  // Si img es null, usar una imagen por defecto
+    img,
     'thumbnail.jpg',
     responseMessage,
     m,
@@ -50,4 +42,5 @@ async function handler(m, { conn: stars, usedPrefix }) {
 handler.command = ['listjadibot', 'bots']
 handler.help = ['bots']
 handler.tags = ['serbot']
+
 export default handler
