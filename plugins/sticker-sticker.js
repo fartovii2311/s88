774b231c6bb34f5187cede923ffed3aa743be9f3
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2024-12-28 10:13:18 -0500

    Update sticker-sticker.js

--- plugins/sticker-sticker.js
+++ plugins/sticker-sticker.js
@@ -1,6 +1,7 @@
-import { sticker } from '../lib/sticker.js';
-import { Sticker } from 'wa-sticker-formatter';
-import uploadFile from '../lib/uploadFile.js';
+import { sticker } from '../lib/sticker.js'
+import uploadFile from '../lib/uploadFile.js'
+import { addExif } from '../lib/sticker.js'
+import { Sticker } from 'wa-sticker-formatter'
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
   try {
