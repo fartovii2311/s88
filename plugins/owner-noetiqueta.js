a1696a8f8e6aded2217c200b169d9562ea5f9e2c
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2024-12-28 12:41:40 -0500

    Update owner-noetiqueta.js

--- plugins/owner-noetiqueta.js
+++ plugins/owner-noetiqueta.js
@@ -4,6 +4,6 @@ await conn.reply(m.chat, `Hola @${m.sender.split`@`[0]} si necesitas la ayuda de
 let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;お;;\nFN:お⁩\nORG:お⁩\nTITLE:\nitem1.TEL;waid=51968382008:51968382008\nitem1.X-ABLabel:おDanịel.xyz⁩\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:お⁩\nEND:VCARD`
 await conn.sendMessage(m.chat, { contacts: { displayName: 'お⁩', contacts: [{ vcard }] }}, {quoted: m})
 }
-handler.customPrefix = /^(@51968382008|@51968382008|@51968382008|@51968382008)$/i
+handler.customPrefix = /^(@51968382008|@51917154203|@51968382008|@51968382008)$/i
 handler.command = new RegExp
 export default handler
