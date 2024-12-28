544efb161d867065a476226059305dabfd7a762d
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2024-12-28 14:47:15 -0500

    Update _premium.js

--- plugins/_premium.js
+++ plugins/_premium.js
@@ -5,13 +5,17 @@ export async function all(m) {
   
   // Evitar que se ejecute en los chats de broadcast
   if (m.chat.endsWith('broadcast')) return
-  
+
   // Comprobar si el usuario tiene tiempo premium activo
   if (user.premiumTime != 0 && user.premium) {
     // Si el tiempo premium ha expirado
     if (new Date() * 1 >= user.premiumTime) {
       // Notificar al usuario que su tiempo premium ha terminado
-      await m.reply(`🚩 @${m.sender.split`@`[0]} tu tiempo como usuario *Premium* ha terminado.`, false, { mentions: [m.sender] })
+      await m.reply(
+        `🚩 @${m.sender.split('@')[0]}, tu tiempo como usuario *Premium* ha terminado.`, 
+        false, 
+        { mentions: [m.sender] }  // Mencionamos al usuario correctamente
+      )
       
       // Desactivar el estado premium del usuario
       user.premiumTime = 0
@@ -24,4 +28,5 @@ handler.help = ['checkpremium'] // Comando de ayuda
 handler.tags = ['dl'] // Categoría del comando
 handler.command = ['checkpremium'] // Nombre del comando
 handler.register = true // Hacer que el handler esté registrado
+
 export default handler
