082904ec6ddc677f8c79d27dd68a9c6f9c401a48
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2024-12-29 01:41:44 -0500

    Update dl-spotify.js

--- plugins/dl-spotify.js
+++ plugins/dl-spotify.js
@@ -1,42 +1,26 @@
-import axios from 'axios';
+// [ ❀ SPOTIFYDL ]
+import fetch from 'node-fetch';
 
-let delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+if (!text) {
+return conn.reply(m.chat, '❀ Ingresa el link de una cancion de spotify', m,rcanal)
+}
 
-let handler = async (m, { conn, args }) => {
-  if (!args || !args[0]) return conn.reply(m.chat, '*\`Ingresa el link del audio a descargar 🤍\`*', m,rcanal);
+try {
+let api = await fetch(`https://api.giftedtech.my.id/api/download/spotifydl?apikey=gifted&url=${text}`)
+let json = await api.json()
+let { quality, title, duration, thumbnail, download_url:dl_url } = json.result
+   
+let HS = `- Titulo : ${title}
+- Calidad : ${quality}
+- Duracion : ${duration}`
 
-  await m.react('🕓');
-  try {
-    let api = await axios.get(`https://deliriussapi-oficial.vercel.app/download/spotifydl?url=${encodeURIComponent(args[0])}`);
-    let json = api.data;
+await conn.sendFile(m.chat, thumbnail, 'HasumiBotFreeCodes.jpg', HS, m)
+await conn.sendFile(m.chat, dl_url, 'HasumiBotFreeCodes.mp3', null, m)
+} catch (error) {
+console.error(error)
+}}
 
-    if (json.status) {
-      let { title, author, image, cover, url } = json.data;
+handler.command = /^(spotifydl)$/i
 
-      let audioGet = await axios.get(url, { responseType: 'arraybuffer' });
-      let audio = audioGet.data;
-
-      let text = `*\`【 S P O T I F Y - D L 】\`*
-
-> *\`TÍTULO:\`* ${title}
-> *\`ARTISTA:\`* ${author}`;
-
-      await m.react('✅'); 
-      await conn.sendFile(m.chat, cover, `cover.jpeg`, text, m,rcanal,fake); 
-      await conn.sendMessage(m.chat, {
-        audio: audio,
-        mimetype: 'audio/mp4',
-        fileName: `Ayo.mp3`,  // Se cambia el nombre del archivo a "Ayo"
-        caption: ``
-      }, { quoted: m }); // Envía el archivo de audio
-    }
-  } catch (error) {
-    console.error(error);
-    await m.react('✖️'); // React con emoji de cruz si ocurre un error
-    m.reply('Hubo un error al intentar descargar el contenido de Spotify.');
-  }
-};
-
-handler.command = /^(spotify)$/i;
-
-export default handler;
+export default handler
