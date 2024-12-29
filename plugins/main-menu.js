1f5ea887463299c4652398cb68e5aaacef262b8d
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2024-12-29 02:25:04 -0500

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -54,7 +54,6 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname, args, command }) => {
     'tools': '𝙷𝚎𝚛𝚛𝚊𝚖𝚒𝚎𝚗𝚝𝚊𝚜',
     'group': '𝙶𝚛𝚞𝚙𝚘𝚜',
     'owner': '𝙾𝚠𝚗𝚎𝚛',
-    'nsfw': 'nsfw',  
   }
 
   try {
