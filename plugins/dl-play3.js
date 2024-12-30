f3955e764e42bda5c4b0b008d00eb33a70d20550
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2024-12-30 10:36:15 -0500

    Update dl-play3.js

--- plugins/dl-play3.js
+++ plugins/dl-play3.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
 import yts from 'yt-search'
 
 let handler = async (m, { conn: star, command, args, text, usedPrefix }) => {
-  if (!text) return m.reply(m.chat, '[ ✰ ] Ingresa el título de un video o canción de *YouTube*.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* Mc Davo - Debes De Saber` ,m, rcanal)
+  if (!text) return conn.reply(m.chat, '[ ✰ ] Ingresa el título de un video o canción de *YouTube*.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* Mc Davo - Debes De Saber` ,m, rcanal)
     await m.react('🕓')
     try {
     let res = await search(args.join(" "))
