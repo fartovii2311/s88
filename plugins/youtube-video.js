838660d330e3df485740c8be80763b0edc187799
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2024-12-30 18:54:28 -0500

    Update youtube-video.js

--- plugins/youtube-video.js
+++ plugins/youtube-video.js
@@ -6,11 +6,7 @@ let handler = async (m, { conn, text, isPrems, isOwner, usedPrefix, command }) =
   }
 
   if (!m.quoted.text.includes("乂  Y O U T U B E  -  P L A Y")) {
-    return conn.reply(
-      m.chat,
-      `[ ✰ ] Etiqueta el mensaje que contenga el resultado de YouTube Play.`,
-      m
-    ).then(() => m.react('✖️'));
+    return conn.reply(m.chat, `[ ✰ ] Etiqueta el mensaje que contenga el resultado de YouTube Play.`, m).then(() => m.react('✖️'));
   }
 
   let urls = m.quoted.text.match(
@@ -27,24 +23,15 @@ let handler = async (m, { conn, text, isPrems, isOwner, usedPrefix, command }) =
     let api = await fetch(`https://restapi.apibotwa.biz.id/api/ytmp4?url=${urls[0]}`);
     let json = await api.json();
 
-    if (!json || !json.data || !json.data.metadata || !json.data.download || !json.data.download.url) {
-      throw new Error('Respuesta inválida de la API. No se encontraron los datos esperados.');
-    }
-
     let title = json.data.metadata.title;
     let dl_url = json.data.download.url;
     let filename = json.data.filename;
 
-    await conn.sendMessage(m.chat, {
-      video: { url: dl_url },
-      fileName: `${filename}.mp4`,
-      mimetype: 'video/mp4'
-    }, { quoted: m });
+    await conn.sendFile(m.chat, dl_url, title + '.mp4', `*» Aquí está tu pedido*`, m, false, { asDocument: user.useDocument });
 
     await m.react('✅');
   } catch (err) {
     console.error(`[Error] ${err.message}`, err);
-    await conn.reply(m.chat, `✰ Hubo un error al intentar descargar el video. Inténtalo nuevamente más tarde.`, m).then(() => m.react('✖️'));
   }
 };
 
