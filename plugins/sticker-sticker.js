df566d01ed95cdfcab1ce827a5078e57b7cf525b
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2024-12-30 18:51:40 -0500

    Update sticker-sticker.js

--- plugins/sticker-sticker.js
+++ plugins/sticker-sticker.js
@@ -1,45 +1,72 @@
+import { sticker } from '../lib/sticker.js'
+import uploadFile from '../lib/uploadFile.js'
+import { addExif } from '../lib/sticker.js'
 import { Sticker } from 'wa-sticker-formatter'
 
-let handler = async (m, { conn }) => {
-  let stiker = false
+let handler = async (m, { conn, args, usedPrefix, command }) => {
   try {
     let q = m.quoted ? m.quoted : m
     let mime = (q.msg || q).mimetype || q.mediaType || ''
-    
-    // Verificar si el mimeType es una imagen
-    if (/image/g.test(mime)) {
+
+    if (/video/g.test(mime)) {
+      // Jalankan kode untuk video di sini
+      if ((q.msg || q).seconds > 10) return m.reply('✧ Máximo 10 segundos.')
       let img = await q.download?.()
-
-      if (!img) return conn.reply(m.chat, `🍁 *_Por favor, intenta enviar una imagen primero._*`, m)
-
+      if (!img) throw m.reply(` `✧ Responde a un Vídeo con el comando*${usedPrefix + command}*` `)
+      let stiker = false
       try {
-        // Crear el sticker usando la imagen descargada
-        const sticker = new Sticker(img, {
-          pack: global.packname,
-          author: global.author,
-          type: 'full'
-        })
-        stiker = await sticker.toBuffer() // Convertir a buffer de sticker
+        stiker = await sticker(img, false, global.stickpack, global.stickauth)
       } catch (e) {
         console.error(e)
+      } finally {
+        if (!stiker) {
+          let out = await uploadFile(img)
+          stiker = await sticker(false, out, global.stickpack, global.stickauth)
+        }
       }
+      conn.sendFile(m.chat, stiker, 'sticker.webp', '', m, null)
+    } else if (/image/g.test(mime)) {
+      // Jalankan kode untuk gambar di sini
+      let [packname, ...author] = args.join` `.split`|`
+      author = (author || []).join`|`
+      let img = await q.download?.()
+      let stiker = false
+      try {
+        let pack = global.stickpack
+        let author = global.stickauth
+        stiker = await addExif(img, pack, author)
+      } catch (e) {
+        console.error(e)
+      } finally {
+        if (!stiker) {
+          stiker = await createSticker(img, false, packname, author)
+        }
+      }
+      m.reply(stiker)
     } else {
-      return m.reply(`💫 *_El archivo no es una imagen válida._*`, m)
+      conn.reply(m.chat, `✧ `Responde a una Imagen o Video *${usedPrefix + command}`*`, m, rcanal)
     }
   } catch (e) {
     console.error(e)
-  } finally {
-    if (stiker) {
-      // Enviar el sticker
-      conn.sendFile(m.chat, stiker, 'sticker.webp', '', m, true)
-    } else {
-      return conn.reply(m.chat, `🌲 *_La conversión ha fallado, intenta enviar una imagen._*`, m)
-    }
+    m.reply('Error')
   }
 }
 
 handler.help = ['sticker']
 handler.tags = ['sticker']
-handler.command = ['sticker']
+
+handler.command = /^s(tic?ker)?(gif)?$/i
+handler.register = true
 
 export default handler
+
+async function createSticker(img, url, packName, authorName, quality) {
+  let stickerMetadata = {
+    type: 'full',
+    pack: stickpack,
+    author: stickauth,
+    quality
+  }
+  return (new Sticker(img ? img : url, stickerMetadata)).toBuffer()
+}
+
