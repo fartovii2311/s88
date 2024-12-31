ebdf944dba229fabd158f0f0b08b736e5066af01
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2024-12-30 19:45:12 -0500

    Update serbot-clear.js

--- plugins/serbot-clear.js
+++ plugins/serbot-clear.js
@@ -1,34 +1,31 @@
 import fs from 'fs';
 import path from 'path';
 
-const handler = async (m, { conn, args }) => {
-  if (args[0]) {
-    try {
-      const nombreUsuario = args[0];  // Usamos directamente el primer argumento
-      const carpetaEliminar = path.join(__dirname, 'serbot', nombreUsuario);
+const handler = async (m, { conn }) => {
+  try {
+    const carpetaEliminar = path.join(__dirname, 'serbot');
 
-      if (fs.existsSync(carpetaEliminar)) {
-        // Eliminar todo el contenido de la carpeta (archivos y subcarpetas)
-        const archivos = fs.readdirSync(carpetaEliminar);
-        archivos.forEach(archivo => {
-          const archivoPath = path.join(carpetaEliminar, archivo);
-          if (fs.lstatSync(archivoPath).isDirectory()) {
-            fs.rmSync(archivoPath, { recursive: true, force: true });
-          } else {
-            fs.unlinkSync(archivoPath);
-          }
-        });
+    if (fs.existsSync(carpetaEliminar)) {
+      // Obtener los archivos y subcarpetas dentro de 'serbot'
+      const archivos = fs.readdirSync(carpetaEliminar);
+      archivos.forEach(archivo => {
+        const archivoPath = path.join(carpetaEliminar, archivo);
+        if (fs.lstatSync(archivoPath).isDirectory()) {
+          // Si es una subcarpeta, eliminarla recursivamente
+          fs.rmSync(archivoPath, { recursive: true, force: true });
+        } else {
+          // Si es un archivo, eliminarlo
+          fs.unlinkSync(archivoPath);
+        }
+      });
 
-        conn.reply(m.chat, `El contenido de la carpeta de usuario ${nombreUsuario} ha sido eliminado exitosamente.`, m);
-      } else {
-        conn.reply(m.chat, `La carpeta de usuario ${nombreUsuario} no existe.`, m);
-      }
-    } catch (err) {
-      console.error(`Error al eliminar el contenido de la carpeta:`, err);
-      conn.reply(m.chat, `Hubo un error al intentar eliminar el contenido de la carpeta.`, m);
+      conn.reply(m.chat, `El contenido dentro de la carpeta 'serbot' ha sido eliminado exitosamente.`, m);
+    } else {
+      conn.reply(m.chat, `La carpeta 'serbot' no existe.`, m);
     }
-  } else {
-    conn.reply(m.chat, 'Por favor, proporciona el nombre del usuario o la carpeta a eliminar.', m);
+  } catch (err) {
+    console.error(`Error al eliminar el contenido de la carpeta:`, err);
+    conn.reply(m.chat, `Hubo un error al intentar eliminar el contenido de la carpeta.`, m);
   }
 };
 
