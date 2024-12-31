06d6a8108c9d54ddb2411d58c16ed24eb36b6da1
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2024-12-31 18:46:34 -0500

    Update nsfw-xnxn.js

--- plugins/nsfw-xnxn.js
+++ plugins/nsfw-xnxn.js
@@ -1,3 +1,7 @@
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+
 handler.help = ['xnxxsearch'].map(v => v + ' <query>');
 handler.tags = ['search'];
 handler.command = ['xnxxsearch', 'xnxxs'];
