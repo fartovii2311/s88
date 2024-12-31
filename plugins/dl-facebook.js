f4da80b9e5d29b61bff31a7d044d0c0e63b7633b
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2024-12-31 09:10:06 -0500

    Update dl-facebook.js

--- plugins/dl-facebook.js
+++ plugins/dl-facebook.js
@@ -2,14 +2,14 @@
 import fetch from 'node-fetch';
 
 let handler = async (m, { conn, text }) => {
-if (!text) return conn.reply(m.chat, `❀ Ingresa un link de facebook`, m)
+if (!text) return conn.reply(m.chat, `❀ Ingresa un link de facebook`, m,rcanal)
 
 try {
 let api = await fetch(`https://api.siputzx.my.id/api/d/facebook?url=${text}`)
 let json = await api.json()
 
-await conn.sendFile(m.chat, json.data.thumbnail, 'HasumiBotFreeCodes.jpg', null, m)
-await conn.sendFile(m.chat, json.data.video, 'HasumiBotFreeCodes.mp4', null, m)
+await conn.sendFile(m.chat, json.data.thumbnail, '.jpg', null, m)
+await conn.sendFile(m.chat, json.data.video, '.mp4', null, m)
 
 } catch (error) {
 console.error(error)
