7e896f733157240cd634632cc98e65e52dc60d10
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2024-12-31 09:30:13 -0500

    Update youtube-video.js

--- plugins/youtube-video.js
+++ plugins/youtube-video.js
@@ -1,6 +1,6 @@
-
-import Starlights from '@StarlightsTeam/Scraper'
+import fetch from 'node-fetch';
 let limit = 300
+
 let handler = async (m, { conn, text, isPrems, isOwner, usedPrefix, command }) => {
 if (!m.quoted) return conn.reply(m.chat, `[ ✰ ] Etiqueta el mensaje que contenga el resultado de YouTube Play.`, m, rcanal).then(_ => m.react('✖️'))
 if (!m.quoted.text.includes("乂  Y O U T U B E  -  P L A Y")) return conn.reply(m.chat, `[ ✰ ] Etiqueta el mensaje que contenga el resultado de YouTube Play.`, m, rcanal).then(_ => m.react('✖️'))
@@ -11,12 +11,11 @@ let user = global.db.data.users[m.sender]
 
 await m.react('🕓')
 try {
-let v = urls[0]
-let { title, size, quality, thumbnail, dl_url } = await Starlights.ytmp4(v)
 
+  
 if (size.split('MB')[0] >= limit) return m.reply(`El archivo pesa mas de ${limit} MB, se canceló la Descarga.`).then(_ => m.react('✖️'))
 
-await conn.sendFile(m.chat, dl_url, title + '.mp4', `*» Título* : ${title}\n*» Calidad* : ${quality}`, m, false, { asDocument: user.useDocument })
+
 await m.react('✅')
 } catch {
 await m.react('✖️')
