cc59aa419180f74201c70eb7a338880e983e20e7
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2024-12-31 14:04:51 -0500

    Update dl-mediafire.js

--- plugins/dl-mediafire.js
+++ plugins/dl-mediafire.js
@@ -8,11 +8,13 @@ try {
 let api = await fetch(`https://restapi.apibotwa.biz.id/api/mediafire?url=${text}`)
 let json = await api.json()
 let { filename, type, size, uploaded, ext, mimetype, download:dl_url } = json.data.response
-m.reply(`⇏ 𝙼𝙴𝙳𝙸𝙰𝙵𝙸𝚁𝙴
+conn.reply(m.chat,`⇏ 𝙼𝙴𝙳𝙸𝙰𝙵𝙸𝚁𝙴
+\n
 - Titulo:* ${filename}
 - *Tipo :* ${type}
 - *Tamaño :* ${size}
-- *Creado :* ${uploaded}`)
+- *Creado :* ${uploaded}`,m,rcanal,fake)
+  
 await conn.sendFile(m.chat, dl_url, filename, null, m,rcanal,fake, null, { mimetype: ext, asDocument: true })
 
 } catch (error) {
