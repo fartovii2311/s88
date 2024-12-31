3928d216e89dd7c37c2cf23e2d4d0cf52ea639bd
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2024-12-31 18:34:41 -0500

    Update youtube-video.js

--- plugins/youtube-video.js
+++ plugins/youtube-video.js
@@ -1,37 +1,42 @@
 import fetch from 'node-fetch';
+
 const limit = 300;
 
 let handler = async (m, { conn, text, isPrems, isOwner, usedPrefix, command }) => {
   if (!m.quoted) {
-    return conn.reply(m.chat, `[ ✰ ] Etiqueta el mensaje que contenga el resultado de YouTube Play.`, m,rcanal)
+    return conn.reply(m.chat, `[ ✰ ] Etiqueta el mensaje que contenga el resultado de YouTube Play.`, m)
       .then(() => m.react('✖️'));
   }
+
   if (!m.quoted.text.includes("乂  Y O U T U B E  -  P L A Y")) {
-    return conn.reply(m.chat, `[ ✰ ] Etiqueta el mensaje que contenga el resultado de YouTube Play.`, m,rcanal)
+    return conn.reply(m.chat, `[ ✰ ] Etiqueta el mensaje que contenga el resultado de YouTube Play.`, m)
       .then(() => m.react('✖️'));
   }
 
   let urls = m.quoted.text.match(/(?:https?:\/\/)?(?:youtu\.be\/|(?:www\.|m\.)?youtube\.com\/(?:watch|v|embed|shorts)(?:\.php)?(?:\?.*v=|\/))([a-zA-Z0-9\_-]+)/gi);
-  if (!urls) return conn.reply(m.chat, `Resultado no Encontrado.`, m,rcanal).then(() => m.react('✖️'));
+  if (!urls) return conn.reply(m.chat, `Resultado no Encontrado.`, m).then(() => m.react('✖️'));
   if (urls.length < text) return conn.reply(m.chat, `Resultado no Encontrado.`, m).then(() => m.react('✖️'));
 
   let user = global.db.data.users[m.sender];
   await m.react('🕓');
 
   try {
-    let api = await fetch(`https://restapi.apibotwa.biz.id/api/ytmp4?url=${urls[0]}`);
+    let api = await fetch(`https://api.giftedtech.my.id/api/download/dlmp4?apikey=gifted&url=${urls[0]}`);
     let json = await api.json();
-    let title = json.data.metadata.title;
-    let dl_url = json.data.download.url;
+
+    let { quality, title, download_url } = json.result;
+
     let size = parseFloat(json.data.metadata.size.split('MB')[0]);
 
     if (size >= limit) {
       return m.reply(`El archivo pesa más de ${limit} MB, se canceló la Descarga.`).then(() => m.react('✖️'));
     }
 
-    await conn.sendMessage(m.chat, { video: { url: dl_url }, fileName: `${json.data.filename}.mp4`, mimetype: "video/mp4" }, { quoted: m });
+    // Send the video
+    await conn.sendMessage(m.chat, { video: { url: download_url }, fileName: `${json.data.filename}.mp4`, mimetype: "video/mp4" }, { quoted: m });
     await m.react('✅');
-  } catch {
+  } catch (error) {
+    console.error(error);
     await m.react('✖️');
   }
 };
