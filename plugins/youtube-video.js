9528b23ed0518612a695e3679282cbe425f1583d
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2024-12-31 00:07:52 -0500

    Update youtube-video.js

--- plugins/youtube-video.js
+++ plugins/youtube-video.js
@@ -1,43 +1,30 @@
-import fetch from 'node-fetch';
 
+import Starlights from '@StarlightsTeam/Scraper'
+let limit = 300
 let handler = async (m, { conn, text, isPrems, isOwner, usedPrefix, command }) => {
-  if (!m.quoted) {
-    return conn.reply(m.chat, `[ ✰ ] Etiqueta el mensaje que contenga el resultado de YouTube Play.`, m, rcanal).then(() => m.react('✖️'));
-  }
+if (!m.quoted) return conn.reply(m.chat, `[ ✰ ] Etiqueta el mensaje que contenga el resultado de YouTube Play.`, m, rcanal).then(_ => m.react('✖️'))
+if (!m.quoted.text.includes("乂  Y O U T U B E  -  P L A Y")) return conn.reply(m.chat, `[ ✰ ] Etiqueta el mensaje que contenga el resultado de YouTube Play.`, m, rcanal).then(_ => m.react('✖️'))
+let urls = m.quoted.text.match(new RegExp(/(?:https?:\/\/)?(?:youtu\.be\/|(?:www\.|m\.)?youtube\.com\/(?:watch|v|embed|shorts)(?:\.php)?(?:\?.*v=|\/))([a-zA-Z0-9\_-]+)/, 'gi'))
+if (!urls) return conn.reply(m.chat, `Resultado no Encontrado.`, m, rcanal).then(_ => m.react('✖️'))
+if (urls.length < text) return conn.reply(m.chat, `Resultado no Encontrado.`, m, rcanal).then(_ => m.react('✖️'))
+let user = global.db.data.users[m.sender]
 
-  if (!m.quoted.text.includes("乂  Y O U T U B E  -  P L A Y")) {
-    return conn.reply(m.chat, `[ ✰ ] Etiqueta el mensaje que contenga el resultado de YouTube Play.`, m, rcanal).then(() => m.react('✖️'));
-  }
+await m.react('🕓')
+try {
+let v = urls[0]
+let { title, size, quality, thumbnail, dl_url } = await Starlights.ytmp4(v)
 
-  let urls = m.quoted.text.match(
-    /(?:https?:\/\/)?(?:youtu\.be\/|(?:www\.|m\.)?youtube\.com\/(?:watch|v|embed|shorts)(?:\.php)?(?:\?.*v=|\/))([a-zA-Z0-9_-]+)/gi
-  );
+if (size.split('MB')[0] >= limit) return m.reply(`El archivo pesa mas de ${limit} MB, se canceló la Descarga.`).then(_ => m.react('✖️'))
 
-  if (!urls) {
-    return conn.reply(m.chat, `Resultado no Encontrado.`, m, rcanal).then(() => m.react('✖️'));
-  }
+await conn.sendFile(m.chat, dl_url, title + '.mp4', `*» Título* : ${title}\n*» Calidad* : ${quality}`, m, false, { asDocument: user.useDocument })
+await m.react('✅')
+} catch {
+await m.react('✖️')
+}}
+handler.help = ['Video']
+handler.tags = ['downloader']
+handler.customPrefix = /^(Video|video|vídeo|Vídeo)/
+handler.command = new RegExp
+//handler.limit = 1
 
-  await m.react('🕓');
-
-  try {
-    let api = await fetch(`https://restapi.apibotwa.biz.id/api/ytmp4?url=${urls[0]}`);
-    let json = await api.json();
-
-    let title = json.data.metadata.title;
-    let dl_url = json.data.download.url;
-    let filename = json.data.filename;
-
-    await conn.sendFile(m.chat, dl_url, title + '.mp4', `*» Aquí está tu pedido*`, m);
-
-    await m.react('✅');
-  } catch (err) {
-    console.error(`[Error] ${err.message}`, err);
-  }
-};
-
-handler.help = ['Video'];
-handler.tags = ['downloader'];
-handler.customPrefix = /^(Video|video|vídeo|Vídeo)/;
-handler.command = new RegExp;
-
-export default handler;
+export default handler
