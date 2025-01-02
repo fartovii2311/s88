ef22a4ed576f2d39221a4cddbc5c355a7a32dec5
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2025-01-02 11:06:14 -0500

    Update owner-salir.js

--- plugins/owner-salir.js
+++ plugins/owner-salir.js
@@ -1,12 +1,30 @@
 let handler = async (m, { conn, text, command }) => {
-let id = text ? text : m.chat  
-let pp = 'https://telegra.ph/file/5ab1ca8bf65c1ddb36c20.mp4'
-await conn.sendMessage(m.chat, { video: { url: pp }, gifPlayback: true, caption: '*Adios a todos, el Bot se despide! (≧ω≦)ゞ*', mentions: [m.sender] }, { quoted: estilo })
-await conn.groupLeave(id)}
-handler.help = ['salir']
-handler.tags = ['owner']
-handler.command = /^(salir|out|leavegc|leave|salirdelgrupo)$/i
-handler.group = true
-handler.rowner = true
+  let id = text ? text : m.chat;
 
-export default handler
+  let pp = 'https://telegra.ph/file/5ab1ca8bf65c1ddb36c20.mp4';
+
+  try {
+    await conn.sendMessage(
+      m.chat,
+      {
+        video: { url: pp },
+        gifPlayback: true,
+        caption: '*Adiós a todos, el Bot se despide! (≧ω≦)ゞ*',
+        mentions: [m.sender],
+      },
+      { quoted: m }
+    );
+
+    await conn.groupLeave(id);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+handler.help = ['salir'];
+handler.tags = ['owner'];
+handler.command = /^(salir|out|leavegc|leave|salirdelgrupo)$/i;
+handler.group = true; 
+handler.rowner = true;
+
+export default handler;
