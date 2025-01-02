749c160bf100b806340c32d56ea47720ecc21e7b
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2025-01-02 17:46:58 -0500

    Update youtube-video.js

--- plugins/youtube-video.js
+++ plugins/youtube-video.js
@@ -4,11 +4,11 @@ const limit = 300 * 1024 * 1024;
 
 let handler = async (m, { conn, text, isPrems, isOwner, usedPrefix, command }) => {
   if (!m.quoted) {
-    return conn.reply(m.chat,`[ ✰ ] Etiqueta el mensaje que contenga el resultado de YouTube Play.`,m,rcanal).then(() => m.react('✖️'));
+    return conn.reply(m.chat, `[ ✰ ] Etiqueta el mensaje que contenga el resultado de YouTube Play.`, m, rcanal).then(() => m.react('✖️'));
   }
 
   if (!m.quoted.text.includes("乂  Y O U T U B E  -  P L A Y")) {
-    return conn.reply(m.chat,`[ ✰ ] Etiqueta el mensaje que contenga el resultado de YouTube Play.`,m,rcanal).then(() => m.react('✖️'));
+    return conn.reply(m.chat, `[ ✰ ] Etiqueta el mensaje que contenga el resultado de YouTube Play.`, m, rcanal).then(() => m.react('✖️'));
   }
 
   let urls = m.quoted.text.match(/(?:https?:\/\/)?(?:youtu\.be\/|(?:www\.|m\.)?youtube\.com\/(?:watch|v|embed|shorts)(?:\.php)?(?:\?.*v=|\/))([a-zA-Z0-9\_-]+)/gi);
@@ -27,16 +27,14 @@ let handler = async (m, { conn, text, isPrems, isOwner, usedPrefix, command }) =
     let json = await api.json();
 
     if (!json.success) {
-      return conn.reply(m.chat,`Error al obtener el video. Intenta de nuevo más tarde.`,m).then(() => m.react('✖️'));
+      return conn.reply(m.chat, `Error al obtener el video. Intenta de nuevo más tarde.`, m, rcanal).then(() => m.react('✖️'));
     }
 
     let { quality, title, download_url } = json.result;
 
-    // Obtener el tamaño del archivo
-    let response = await fetch(download_url);
-    let size = Number(response.headers.get('content-length')) || 0;
+    let headResponse = await fetch(download_url, { method: 'HEAD' });
+    let size = Number(headResponse.headers.get('content-length')) || 0;
 
-    // Determinar cómo enviar el archivo
     if (size > limit) {
       await conn.sendMessage(m.chat, {
         document: { url: download_url },
