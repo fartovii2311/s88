0cbf0697c1260d3df0e6e017ff693a7f2f50b660
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2025-01-05 17:32:33 -0500

    Update dl-play2.js

--- plugins/dl-play2.js
+++ plugins/dl-play2.js
@@ -1,10 +1,10 @@
 import fetch from 'node-fetch';
 import yts from 'yt-search';
+import ytdl from 'ytdl-core';
 
 let handler = async (m, { conn, text, args }) => {
   if (!text) return m.reply("❀ Ingresa un texto de lo que quieres buscar");
   
-
   // Realizar la búsqueda en YouTube
   let ytres = await search(args.join(" "));
   let txt = `- Título: ${ytres[0].title}
@@ -14,21 +14,24 @@ let handler = async (m, { conn, text, args }) => {
 - Url: ${'https://youtu.be/' + ytres[0].videoId}`;
 
   // Enviar la imagen de la miniatura y la información del video
-  await conn.sendFile(m.chat, ytres[0].image, 'thumbnail.jpg', txt, m,rcanal,fake);
+  await conn.sendFile(m.chat, ytres[0].image, 'thumbnail.jpg', txt, m);
 
   try {
-    // Descargar el video desde el enlace
-    let api = await fetch(`https://api.giftedtech.my.id/api/download/dlmp4?apikey=gifted&url=${ytres[0].url}`);
-    let json = await api.json();
-    let { quality, title, download_url } = json.result;
+    // Descargar el video usando ytdl-core
+    let videoUrl = 'https://youtu.be/' + ytres[0].videoId;
+    let info = await ytdl.getInfo(videoUrl);
+    
+    // Filtrar los mejores formatos de video
+    let videoFormat = ytdl.chooseFormat(info.formats, { filter: 'audioandvideo' });
 
     // Enviar el video descargado
     await conn.sendMessage(m.chat, {
-      video: { url: download_url },
-      caption: `${title}`,
+      video: { url: videoFormat.url },
+      caption: ytres[0].title,
       mimetype: 'video/mp4',
-      fileName: `${title}.mp4`
+      fileName: `${ytres[0].title}.mp4`
     }, { quoted: m });
+
   } catch (error) {
     console.error('Error al obtener el video:', error);
     m.reply('❀ Ocurrió un error al intentar obtener el video. Intenta nuevamente.');
