ed679d54763becaec460ab357bfae5029ecb41e8
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2025-01-05 11:14:59 -0500

    Update youtube-audio.js

--- plugins/youtube-audio.js
+++ plugins/youtube-audio.js
@@ -1,31 +1,48 @@
+import core from 'NakanoTeam-Scraper'
 
-import Starlights from '@StarlightsTeam/Scraper'
-let limit = 200
+let limit = 200 // Limite en MB
 
 let handler = async (m, { conn, text, isPrems, isOwner, usedPrefix, command }) => {
-if (!m.quoted) return conn.reply(m.chat, `[ ✰ ] Etiqueta el mensaje que contenga el resultado de YouTube Play.`, m, rcanal).then(_ => m.react('✖️'))
-if (!m.quoted.text.includes("乂  Y O U T U B E  -  P L A Y")) return conn.reply(m.chat, `[ ✰ ] Etiqueta el mensaje que contenga el resultado de YouTube Play.`, m, rcanal).then(_ => m.react('✖️'))
-let urls = m.quoted.text.match(new RegExp(/(?:https?:\/\/)?(?:youtu\.be\/|(?:www\.|m\.)?youtube\.com\/(?:watch|v|embed|shorts)(?:\.php)?(?:\?.*v=|\/))([a-zA-Z0-9\_-]+)/, 'gi'))
-if (!urls) return conn.reply(m.chat, `Resultado no Encontrado.`, m, rcanal).then(_ => m.react('✖️'))
-if (urls.length < text) return conn.reply(m.chat, `Resultado no Encontrado.`, m, rcanal).then(_ => m.react('✖️'))
-let user = global.db.data.users[m.sender]
-	
-await m.react('🕓')
-try {
-let v = urls[0]
-let { title, size, quality, thumbnail, dl_url } = await Starlights.ytmp3(v)
+  if (!m.quoted) {
+    return conn.reply(m.chat, `[ ✰ ] Etiqueta el mensaje que contenga el resultado de YouTube Play.`, m, rcanal).then(_ => m.react('✖️'))
+  }
+  
+  if (!m.quoted.text.includes("乂  Y O U T U B E  -  P L A Y")) {
+    return conn.reply(m.chat, `[ ✰ ] Etiqueta el mensaje que contenga el resultado de YouTube Play.`, m, rcanal).then(_ => m.react('✖️'))
+  }
+  
+  let urls = m.quoted.text.match(new RegExp(/(?:https?:\/\/)?(?:youtu\.be\/|(?:www\.|m\.)?youtube\.com\/(?:watch|v|embed|shorts)(?:\.php)?(?:\?.*v=|\/))([a-zA-Z0-9\_-]+)/, 'gi'))
+  
+  if (!urls || urls.length < text) {
+    return conn.reply(m.chat, `Resultado no Encontrado.`, m, rcanal).then(_ => m.react('✖️'))
+  }
 
-if (size.split('MB')[0] >= limit) return m.reply(`El archivo pesa mas de ${limit} MB, se canceló la Descarga.`).then(_ => m.react('✖️'))
+  let user = global.db.data.users[m.sender]
+  
+  await m.react('🕓')
+  
+  try {
+    let v = urls[0]
+    let { title, size, quality, thumbnail, dl_url } = await core.ytmp3(v)
+    
+    // Convertir el tamaño del archivo a MB (asumiendo que el tamaño está en un formato como '10MB')
+    let fileSizeMB = parseFloat(size.split('MB')[0])
 
-await conn.sendFile(m.chat, dl_url, title + '.mp3', null, m, false, { mimetype: 'audio/mpeg', asDocument: user.useDocument })
-await m.react('✅')
-} catch {
-await m.react('✖️')
-}}
+    if (fileSizeMB >= limit) {
+      return m.reply(`El archivo pesa más de ${limit} MB, se canceló la descarga.`).then(_ => m.react('✖️'))
+    }
+
+    await conn.sendFile(m.chat, dl_url, title + '.mp3', null, m, false, { mimetype: 'audio/mpeg', asDocument: user.useDocument })
+    await m.react('✅')
+  } catch (error) {
+    console.error(error)
+    await m.react('✖️')
+  }
+}
+
 handler.help = ['Audio']
 handler.tags = ['downloader']
 handler.customPrefix = /^(Audio|audio)/
 handler.command = new RegExp
-//handler.limit = 1
 
 export default handler
