632bc8ebc93750aee5f618dcddc74027d6ab8e79
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2025-01-05 11:25:08 -0500

    Update dl-play.js

--- plugins/dl-play.js
+++ plugins/dl-play.js
@@ -1,42 +1,52 @@
-
-import { youtube } from 'btch-downloader'
-import yts from 'yt-search'
-import axios from 'axios'
+import { youtube } from 'btch-downloader';
+import yts from 'yt-search';
+import axios from 'axios';
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) return conn.reply(m.chat, ❀ Ingresa el texto de lo que quieres buscar, m)
+  if (!text) return conn.reply(m.chat, '❀ Ingresa el texto de lo que quieres buscar', m);
 
-try {
-let ytsres = await yts(text)
-let video = ytsres.videos[0]
+  try {
+    let ytsres = await yts(text);
+    let video = ytsres.videos[0];
 
-if (!video) return conn.reply(m.chat, ❀ Sin resultados obtenidos :(, m)
+    if (!video) return conn.reply(m.chat, '❀ Sin resultados obtenidos :(', m);
 
-let { title, duration, views, ago, author, thumbnail, url } = video
-let HS = `- Titulo : ${title}
-- Duracion : ${duration.timestamp}
-- Visitas : ${views.toLocaleString()}
-- Subido : ${ago}
-- Autor : ${author.name}`
+    let { title, duration, views, ago, author, thumbnail, url } = video;
+    let HS = `- Titulo: ${title}
+- Duracion: ${duration.timestamp}
+- Visitas: ${views.toLocaleString()}
+- Subido: ${ago}
+- Autor: ${author.name}`;
 
- 
-await conn.sendMessage(m.chat, {text: HS,
-contextInfo: { externalAdReply: {
-title: ${title}, body: ${author.name},
-thumbnailUrl: thumbnail, sourceUrl: url,
-mediaType: 1, renderLargerThumbnail: true
-}}}, { quoted: m })
+    await conn.sendMessage(m.chat, {
+      text: HS,
+      contextInfo: {
+        externalAdReply: {
+          title: `${title}`,
+          body: `${author.name}`,
+          thumbnailUrl: thumbnail,
+          sourceUrl: url,
+          mediaType: 1,
+          renderLargerThumbnail: true
+        }
+      }
+    }, { quoted: m });
 
-let data = await youtube(url)
+    let data = await youtube(url);
 
-if (!data || !data.mp3) return conn.reply(m.chat, ❀ Descarga fallida :(, m)
+    if (!data || !data.mp3) return conn.reply(m.chat, '❀ Descarga fallida :(', m);
 
-await conn.sendMessage(m.chat, { audio: { url: data.mp3 }, mimetype: 'audio/mpeg', }, { quoted: m })
-//data.mp4 para video :v
-} catch (error) {
-console.error(error)
-}}
+    await conn.sendMessage(m.chat, {
+      audio: { url: data.mp3 },
+      mimetype: 'audio/mpeg',
+    }, { quoted: m });
+    // Si quieres enviar el video, usa data.mp4
+  } catch (error) {
+    console.error(error);
+    return conn.reply(m.chat, '❀ Ocurrió un error al procesar la solicitud :(', m);
+  }
+};
 
-handler.command = ['play3']
+handler.command = ['play3'];
 
-export default handler
+export default handler;
