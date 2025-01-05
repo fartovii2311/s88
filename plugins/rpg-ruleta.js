670328094eb82b2a1329b7271d4432f0abecf8d0
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2025-01-04 20:00:57 -0500

    Update and rename game-ruleta.js to rpg-ruleta.js

--- plugins/rpg-ruleta.js
+++ plugins/rpg-ruleta.js
@@ -52,7 +52,7 @@ let handler = async (m, { conn, text, command, usedPrefix }) => {
     
   }, 10000)
 }
-handler.tags = ['game']
+handler.tags = ['rpg']
 handler.help =['ruleta *<cantidad> <color>*']
 handler.command = ['ruleta', 'roulette', 'rt']
 handler.register = true
