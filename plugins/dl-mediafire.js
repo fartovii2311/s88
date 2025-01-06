dda633a08f99a7be8e6b77467b174098e4ebb072
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2025-01-06 17:22:21 -0500

    Update dl-mediafire.js

--- plugins/dl-mediafire.js
+++ plugins/dl-mediafire.js
@@ -1,8 +1,7 @@
-
 import fetch from 'node-fetch';
 
 let handler = async (m, { conn, text }) => {
-    if (!text) return conn.reply(m.chat, `❀ Ingresa un link de mediafire`, m)
+    if (!text) return conn.reply(m.chat, `❀ Ingresa un link de mediafire`, m,rcanal)
   await m.react('🕓');
 
     try {
