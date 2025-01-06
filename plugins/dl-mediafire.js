4582a8994e66f059b2182df8ed74b1ff4ef5f291
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2025-01-05 23:05:38 -0500

    Update dl-mediafire.js

--- plugins/dl-mediafire.js
+++ plugins/dl-mediafire.js
@@ -1,63 +1,28 @@
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-
-// Para obtener el directorio actual, usa fileURLToPath y dirname
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 import fetch from 'node-fetch';
-import fs from 'fs';
-import path from 'path';
-import archiver from 'archiver';
 
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-    if (!text) return conn.reply(m.chat, `❀ Ingresa un link de MediaFire`, m);
+let handler = async (m, { conn, text }) => {
+    if (!text) return conn.reply(m.chat, `❀ Ingresa un link de mediafire`, m)
+  await m.react('🕓');
 
     try {
-        let api = await fetch(`https://restapi.apibotwa.biz.id/api/mediafire?url=${text}`);
-        let json = await api.json();
-        let { filename, type, size, uploaded, ext, mimetype, download: dl_url } = json.data.response;
+        let api = await fetch(`https://restapi.apibotwa.biz.id/api/mediafire?url=${text}`)
+        let json = await api.json()
+        let { filename, type, size, uploaded, ext, mimetype, download: dl_url } = json.data.response
+        m.reply(`*${filename}*
 
-        const sizeInBytes = parseFloat(size) * 1024 * 1024;
-
-        const MAX_SIZE = 50 * 1024 * 1024;  
-        if (sizeInBytes > MAX_SIZE) {
-            const tempDir = path.join(__dirname, 'temp');
-            if (!fs.existsSync(tempDir)) {
-                fs.mkdirSync(tempDir);
-            }
-
-            const tempFilePath = path.join(tempDir, filename);
-            const fileStream = fs.createWriteStream(tempFilePath);
-
-            const res = await fetch(dl_url);
-            res.body.pipe(fileStream);
-
-            fileStream.on('finish', async () => {
-                const zipPath = path.join(tempDir, `${filename}.zip`);
-                const output = fs.createWriteStream(zipPath);
-                const archive = archiver('zip', { zlib: { level: 9 } });
-
-                archive.pipe(output);
-                archive.file(tempFilePath, { name: filename });
-                await archive.finalize();
-
-                await conn.sendFile(m.chat, zipPath, `${filename}.zip`, null, m);
-                fs.unlinkSync(tempFilePath);
-                fs.unlinkSync(zipPath);
-            });
-        } else {
-            await conn.sendFile(m.chat, dl_url, filename, null, m, 'rcanal', false, null, { mimetype: ext, asDocument: true });
-        }
-
+- *Tipo :* ${type}
+- *Tamaño :* ${size}
+- *Creado :* ${uploaded}`)
+        await m.react('✅');
+        await conn.sendFile(m.chat, dl_url, filename, null, m, null, { mimetype: ext, asDocument: true })
     } catch (error) {
-        console.error(error);
-        conn.reply(m.chat, '❗ Ocurrió un error al procesar el archivo', m);
+        console.error(error)
     }
-};
+}
 
-handler.help = ['mediafire'].map(v => v + ' *<url>*');
-handler.tags = ['dl', 'premium'];
-handler.command = ['mediafire', 'mdfire', 'mf'];
+handler.help = ['mediafire *<url>*']
+handler.tags = ['dl']
+handler.command = ['mediafire']
 
 export default handler;
