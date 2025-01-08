591864dfad8871fd4764eb5016fa7feedd79f412
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2025-01-07 20:13:26 -0500

    Update config.js

--- config.js
+++ config.js
@@ -53,6 +53,7 @@ global.dis = ':⁖֟⊱┈֟፝❥'
 global.catalogo = fs.readFileSync('./storage/img/catalogo.png')
 global.miniurl = fs.readFileSync('./storage/img/miniurl.jpg')
 global.icons = fs.readFileSync('./storage/img/miniurl.jpg')
+global.loadDatabase = fs.readFileSync('./storage/database/database.json')
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
