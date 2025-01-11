f108c5c146e7bd7c7741af55373a99c4368dc129
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2025-01-11 07:18:14 -0500

    Update dl-gitclone.js

--- plugins/dl-gitclone.js
+++ plugins/dl-gitclone.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 let regex = /(?:https|git)(?::\/\/|@)github\.com[\/:]([^\/:]+)\/(.+)/i
 let handler = async (m, { args, usedPrefix, command }) => {
   if (!args[0]) {
-    return conn.reply(m.chat, `🚩 Escribe la URL de un repositorio de GitHub que deseas descargar.`, m,rcanal)
+    return conn.reply(m.chat, `[ ᰔᩚ ] Ingresa el url deL repositorio de *GitHub*.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* https://api.github.com/`, m,rcanal)
   }
   if (!regex.test(args[0])) {
     return conn.reply(m.chat, `Verifica que la *URL* sea de GitHub`, m).then(_ => m.react('✖️'))
