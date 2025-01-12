79e96c5a2c881fb393b7cbc4d01e8f9be046b107
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2025-01-12 11:39:14 -0500

    Update _fototeta.js

--- plugins/_fototeta.js
+++ plugins/_fototeta.js
@@ -1,6 +1,5 @@
 let handler = async (m, { conn, usedPrefix, command}) => {
 await conn.reply(m.chat,`Padre nuestro, que estás en los Cielos, santificado sea tu nombre, venga tu Reino, hágase tu voluntad así en la tierra como en el cielo. y perdónanos nuestras deudas así como nosotros perdonamos a nuestros deudores, y no nos dejes caer en la tentación, mas líbranos del mal.`, estilo)
 }
-handler.customPrefix = /^(Fototeta|fototeta)$/i
-handler.command = new RegExp
+handler.command = /^(Fototeta|fototeta)$/i
 export default handler
