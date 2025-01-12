08088e64883c2e79c2f26ac6ebe66126231b650d
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2025-01-12 11:37:45 -0500

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -1,4 +1,4 @@
-const linkRegex = /(chat.whatsapp.com\/(?:invite\/)?([0-9A-Za-z]{20,24})|whatsapp:\/\/send\?text=)([A-Za-z0-9&=]+)/i;
+const linkRegex = /(chat.whatsapp.com\/(?:invite\/)?([0-9A-Za-z]{20,24})|whatsapp\.com\/channel\/[A-Za-z0-9]+)/i;
 
 export async function before(m, { conn, isAdmin, isBotAdmin }) {
     if (m.isBaileys && m.fromMe)
@@ -7,16 +7,16 @@ export async function before(m, { conn, isAdmin, isBotAdmin }) {
 
     let chat = global.db.data.chats[m.chat];
     let bot = global.db.data.settings[this.user.jid] || {};
-    const isGroupLink = linkRegex.exec(m.text);
+    const isGroupOrChannelLink = linkRegex.exec(m.text);
 
-    if (chat.antiLink && isGroupLink) {
+    if (chat.antiLink && isGroupOrChannelLink) {
         if (isBotAdmin) {
             const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`;
             if (m.text.includes(linkThisGroup)) return !0;
         }
 
         await conn.sendMessage(m.chat, { delete: m.key });
-        await conn.reply(m.chat, `🚩 *No permitimos enlaces de otros grupos*, lo siento *@${m.sender.split('@')[0]}*. Serás expulsado del grupo ${isBotAdmin ? '' : '\n\nNo soy admin, así que no te puedo expulsar :('}`, null, { mentions: [m.sender] });
+        await conn.reply(m.chat, `🚩 *No permitimos enlaces de otros grupos o canales*, lo siento *@${m.sender.split('@')[0]}*. Serás expulsado del grupo ${isBotAdmin ? '' : '\n\nNo soy admin, así que no te puedo expulsar :('}`, null, { mentions: [m.sender] });
 
         if (isBotAdmin && chat.antiLink) {
             await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
