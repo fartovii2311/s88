bac20576e2f2d57717d3eaf80c2e156a2b6239ab
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2025-01-12 11:31:06 -0500

    Update info-runtime.js

--- plugins/info-runtime.js
+++ plugins/info-runtime.js
@@ -1,25 +1,29 @@
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-	
-	let _muptime
+    let _muptime;
     if (process.send) {
-      process.send('uptime')
-      _muptime = await new Promise(resolve => {
-        process.once('message', resolve)
-        setTimeout(resolve, 1000)
-      }) * 1000
+        process.send('uptime');
+        _muptime = await new Promise(resolve => {
+            process.once('message', resolve);
+            setTimeout(resolve, 1000);
+        }) * 1000;
     }
-    let muptime = clockString(_muptime)
-   m.reply(`*» Bot activo durante* : ${muptime}`) 
+    let muptime = clockString(_muptime);
+    
+    m.reply(`*» El bot ha estado activo durante:* \n${muptime}`);
 }
+
 handler.help = ['runtime']
 handler.tags = ['main']
 handler.command = ['runtime', 'uptime']
-export default handler
+export default handler;
 
 function clockString(ms) {
-  let d = isNaN(ms) ? '--' : Math.floor(ms / 86400000)
-  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24
-  let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
-  let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-  return [d, 'd ', h, 'h ', m, 'm ', s, 's '].map(v => v.toString().padStart(2, 0)).join('')
+  let d = isNaN(ms) ? '--' : Math.floor(ms / 86400000);
+  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24;
+  let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60;
+  let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60;
+  
+  return [d, 'd ', h, 'h ', m, 'm ', s, 's ']
+    .map(v => v.toString().padStart(2, 0)) 
+    .join('');
 }
