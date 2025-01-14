4da746c4de063c74185fad3d2b94da38eba6d896
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2025-01-14 17:38:18 -0500

    Update info-creator.js

--- plugins/info-creator.js
+++ plugins/info-creator.js
@@ -1,28 +1,9 @@
 import fetch from 'node-fetch';
 
-let handler = async (m, { conn, usedPrefix, text, args, command }) => {
-    m.react('🤍');
+let handler = async (m, { conn }) => {
+await m.react('🤍');
 
-    let fkontak = { 
-        "key": { 
-            "participants": "0@s.whatsapp.net", 
-            "remoteJid": "status@broadcast", 
-            "fromMe": false, 
-            "id": "Halo" 
-        }, 
-        "message": { 
-            "contactMessage": { 
-                "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` 
-            }
-        }, 
-        "participant": "0@s.whatsapp.net" 
-    };
-
-    let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
-    let name = await conn.getName(who);
-    let edtr = `@${m.sender.split`@`[0]}`;
-
-    let vcard = `BEGIN:VCARD
+let vcard = `BEGIN:VCARD
 VERSION:3.0
 N:WhatsApp;
 FN:DARK-CORE 🍃
@@ -33,15 +14,12 @@ ADR:;;🇵🇪 Perú;;;;;;
 BDAY;value=date:🤍 anonimous
 END:VCARD`;
 
-    const tag_own = await conn.sendMessage(m.chat, { 
+    await conn.sendMessage(m.chat, { 
         contacts: { 
             displayName: 'Owner Contact', 
             contacts: [{ vcard }] 
         } 
-    }, { quoted: fkontak });
-
-    let caption = `*👋 Hola ${edtr}, este es el contacto de mi creador 🤍*`;
-    await conn.reply(m.chat, caption, tag_own, { mentions: conn.parseMention(caption) });
+    });
 };
 
 handler.help = ['owner', 'creator'];
