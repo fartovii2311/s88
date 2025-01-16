4c49aec41a34707218549b93ed17f9d3925ca302
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2025-01-16 00:55:09 -0500

    Update info-creator.js

--- plugins/info-creator.js
+++ plugins/info-creator.js
@@ -1,9 +1,9 @@
 import fetch from 'node-fetch';
 
 let handler = async (m, { conn }) => {
-await m.react('🤍');
+    await m.react('🤍'); // Reacciona al mensaje con un emoji
 
-let vcard = `BEGIN:VCARD
+    let vcard = `BEGIN:VCARD
 VERSION:3.0
 N:WhatsApp;
 FN:DARK-CORE 🍃
@@ -14,12 +14,13 @@ ADR:;;🇵🇪 Perú;;;;;;
 BDAY;value=date:🤍 anonimous
 END:VCARD`;
 
-await conn.sendMessage(m.chat, { 
+    // Envía el mensaje con el contacto
+    await conn.sendMessage(m.chat, { 
         contacts: { 
             displayName: 'Owner Contact', 
-            contacts: [{ vcard }] 
-        },{ quoted: m }
-    });
+            contacts: [{ vcard }]
+        }
+    }, { quoted: m }); // Este parámetro debe ir fuera del objeto principal
 };
 
 handler.help = ['owner', 'creator'];
@@ -27,4 +28,3 @@ handler.tags = ['main'];
 handler.command = /^(owner|creator|creador|dueño)$/i;
 
 export default handler;
-
