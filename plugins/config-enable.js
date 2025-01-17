4bd0db516861e0ff6aed443fd37c2711265d31a4
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2025-01-17 07:16:30 -0500

    Update config-enable.js

--- plugins/config-enable.js
+++ plugins/config-enable.js
@@ -308,7 +308,7 @@ let handler = async (m, { conn, usedPrefix, command, args, isOwner, isAdmin, isR
 │ ➤ Detect: ${chat.detect ? '✅ Activado' : '❌ Desactivado'}
 │ ➤ ModoAdmin: ${chat.modoadmin ? '✅ Activado' : '❌ Desactivado'}
 │ ➤ NSFW: ${chat.modohorny ? '✅ Activado' : '❌ Desactivado'}
-│ ➤ AntiLink: ${chat.antilink ? '✅ Activado' : '❌ Desactivado'}
+│ ➤ AntiLink:* ${chat.antiLink ? '✅ Activado' : '❌ Desactivado'}
 ╰───────────────╯
 
 ╭───────────────✦
