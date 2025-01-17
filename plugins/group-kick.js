3e3188adc08f0fc06655af48a96ec03dfbf92602
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2025-01-17 14:43:35 -0500

    Update group-kick.js

--- plugins/group-kick.js
+++ plugins/group-kick.js
@@ -1,11 +1,13 @@
 let handler = async (m, { conn, participants, usedPrefix, command, isROwner }) => {
-    let kickte = `🚩 Menciona al usuario que deseas eliminar.`;
-
-    if (!m.mentionedJid[0] && !m.quoted) 
-        return m.reply(kickte, m.chat, { mentions: conn.parseMention(kickte) });
+    // Verifica si el mensaje es válido
+    if (!m || !m.isGroup || !m.mentionedJid[0] && !m.quoted) {
+        return m.reply(`🚩 Menciona al usuario que deseas eliminar.`, m.chat);
+    }
 
     let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender;
-    
+    let ownerJid = m.chat.split`-`[0] + '51968382008@s.whatsapp.net';
+
+    // Verifica si el usuario es un creador (global.owner)
     if (global.owner.includes(user)) {
         return conn.reply(m.chat, `🚩 No puedo eliminar a un creador del bot porque es mi creador.`, null, { mentions: [user] });
     }
