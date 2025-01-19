55559f85865fe1d3c6ee3d620c83fd6e4c07550a
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2025-01-19 15:14:34 -0500

    Update prueba2.js

--- plugins/prueba2.js
+++ plugins/prueba2.js
@@ -4,14 +4,16 @@ let handler = async (m, { conn }) => {
         contextInfo: {
             externalAdReply: {
                 title: 'Grupo • SKY - Publicidad (no bots)',
-                body: '',
-                mediaType: 1,
-                renderLargerThumbnail: false
+                body: '', // Subtítulo vacío
+                mediaType: 1, // Tipo de contenido
+                renderLargerThumbnail: false, // Evitar imagen
+                url: false // Evitar que aparezca la URL
             }
         }
     };
-    console.log('Enviando mensaje...'); 
+
     await conn.sendMessage(m.chat, mensaje, { quoted: m });
 };
+
 handler.command = ['grupopubli']; // Comando para ejecutar
 export default handler;
