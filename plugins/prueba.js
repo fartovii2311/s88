69b74a6a7047dedb70ae20fea4738fce6a106e47
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2025-01-19 12:55:54 -0500

    Update prueba.js

--- plugins/prueba.js
+++ plugins/prueba.js
@@ -2,22 +2,29 @@ import fetch from 'node-fetch';
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
     if (!text) return conn.reply(m.chat, '❀ Ingresa un link de youtube', m);
-    
+
     try {
         await m.react('🕒');
-        let api = await fetch(https://apidl.asepharyana.cloud/api/downloader/ytmp4?url=${text}&quality=360);
+        let api = await fetch(`https://apidl.asepharyana.cloud/api/downloader/ytmp4?url=${text}&quality=360`);
         let json = await api.json();
         let { title, author, authorUrl, lengthSeconds, views, uploadDate, thumbnail, description, duration, downloadUrl, quality } = json;
         
-        let HS = *Titulo :* ${title}\nDuración : ${duration}\nCalidad : ${quality}p;
+        let HS = `*Titulo :* ${title}\nDuración : ${duration}\nCalidad : ${quality}p`;
 
-        let durationInSeconds = parseInt(duration.split(':')[0]) * 60 + parseInt(duration.split(':')[1]);
+        // Asegúrate de que el formato de duración esté correcto (hh:mm)
+        let durationInSeconds = 0;
+        if (duration.includes(':')) {
+            let timeParts = duration.split(':');
+            durationInSeconds = parseInt(timeParts[0]) * 60 + parseInt(timeParts[1]);
+        } else {
+            durationInSeconds = parseInt(duration); // Si es solo en segundos
+        }
 
         if (durationInSeconds >= 2400) {
             await conn.sendMessage(m.chat, { 
                 document: { url: downloadUrl }, 
                 mimetype: 'video/mp4', 
-                fileName: ${title}.mp4, 
+                fileName: `${title}.mp4`, 
                 caption: HS 
             }, { quoted: m });
         } else {
