f2edfe2d04b07633d69488e809e4003fd8bdf893
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2025-01-19 15:18:40 -0500

    Update prueba2.js

--- plugins/prueba2.js
+++ plugins/prueba2.js
@@ -2,16 +2,17 @@ let handler = async (m, { conn }) => {
     let mensaje = {
         text: '*Este es un mensaje de ejemplo.*',
         contextInfo: {
+            isForwarded: true,
             externalAdReply: {
                 title: 'Grupo • SKY - Publicidad (no bots)', // Título
+                newsletterJid: "120363371366801178@newsletter",
                 mediaType: 1,
                 renderLargerThumbnail: false,
                 url: false
             }
         }
     };
-
-    await conn.sendMessage(m.chat, mensaje, { quoted: m });
+  await conn.sendMessage(m.chat, mensaje, { quoted: m });
 };
 
 handler.command = ['grupopubli']; // Comando para ejecutar
