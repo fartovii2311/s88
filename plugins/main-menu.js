3846180950542672fd076e15eb26bbc2000747b2
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2025-01-19 10:54:23 -0500

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -47,8 +47,10 @@ let loadd = [
 let { key } = await conn.reply(m.chat, { text: 'ʟ ᴏ ᴀ ᴅ ɪ ɴ ɢ. . .' });
 
 for (let i = 0; i < loadd.length; i++) {
-  await new Promise(resolve => setTimeout(resolve, 850));
-  await conn.reply(m.chat, { text: loadd[i], edit: key });
+    await new Promise(resolve => setTimeout(resolve, 850));
+    await conn.reply(m.chat, { text: loadd[i], edit: key });
+}
+
 
 const defaultMenu = {
   before: `*\`ʜᴏʟᴀ\`* %name *\`sᴏʏ ʟʏɴx - ᴀɪ ᴛᴜ ᴀsɪsᴛᴇɴᴛᴇ ᴠɪʀᴛᴜᴀʟ ᴄʀᴇᴀᴅᴏ ᴘᴏʀ ᴅᴀʀᴋᴄᴏʀᴇ , ᴇsᴛᴇ ᴇs ʀʟ ᴍᴇɴᴜ ᴄᴏᴍᴘʟᴇᴛᴏ ˙˚ʚ₍ ᐢ. ̫ .ᐢ ₎ɞ˚\`*
