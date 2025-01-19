f6c1b15d5e2b478b755fff0e99579e60611cf873
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2025-01-19 15:13:54 -0500

    Update prueba2.js

--- plugins/prueba2.js
+++ plugins/prueba2.js
@@ -1,18 +1,17 @@
 let handler = async (m, { conn }) => {
     let mensaje = {
-        text: '*Este es un mensaje de ejemplo.*', // Mensaje principal
+        text: '*Este es un mensaje de ejemplo.*',
         contextInfo: {
             externalAdReply: {
-                title: 'Grupo • SKY - Publicidad (no bots)', // Título destacado
-                body: '', // Subtítulo (puedes dejarlo vacío)
-                mediaType: 1, // Tipo de contenido
-                renderLargerThumbnail: false // Evitar que aparezca una imagen
+                title: 'Grupo • SKY - Publicidad (no bots)',
+                body: '',
+                mediaType: 1,
+                renderLargerThumbnail: false
             }
         }
     };
-
+    console.log('Enviando mensaje...'); 
     await conn.sendMessage(m.chat, mensaje, { quoted: m });
 };
-
 handler.command = ['grupopubli']; // Comando para ejecutar
 export default handler;
