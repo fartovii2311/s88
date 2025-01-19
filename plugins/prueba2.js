fdf9e30eb8311a15141a7ffce6fc41ff03a9a1cd
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2025-01-19 15:24:24 -0500

    Update prueba2.js

--- plugins/prueba2.js
+++ plugins/prueba2.js
@@ -1,11 +1,7 @@
 let handler = async (m, { conn }) => {
     let mensaje = {
-        text: '*Este es un mensaje de ejemplo.*',
-        contextInfo: {
-            externalAdReply: {
-                title: 'Grupo • SKY - Publicidad (no bots)',
-            }
-        }
+        text: '*Este es un mensaje de ejemplo.*', // Mensaje de texto
+        title: 'Grupo • SKY - Publicidad (no bots)', // Solo el título
     };
 
     await conn.sendMessage(m.chat, mensaje, { quoted: m });
