2f4b83affee19953107c690d49f725a170578077
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2025-01-20 12:23:04 -0500

    Update serbot-bots.js

--- plugins/serbot-bots.js
+++ plugins/serbot-bots.js
@@ -21,7 +21,7 @@ async function handler(m, { conn: stars, usedPrefix }) {
 
   let message = users.map((v, index) => `
 *[ \`${index + 1}\` - ${v.user.name || 'Sin Nombre'} ]*
-🤍 *Link:* https://wa.me/${v.user.jid.replace(/[^0-9]/g, '')}.code
+🤍 *Link:* https://wa.me/${v.user.jid.replace(/[^0-9]/g, '')}?text=.code
 `).join('\n\n')
 
   let responseMessage = `🟢 *Subbots Activos: ${totalUsers}*\n\n${message.trim() || '_No hay subbots activos en este momento._'}`
