dc429fe2a5f362f8f15dde830c8dc42861280667
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2025-01-19 22:43:07 -0500

    Update and rename serbot-delserbot.js to serbot-qrdell.js

---
+++ plugins/serbot-qrdell.js
@@ -0,0 +1,29 @@
+import { promises as fs } from "fs";
+
+let handler = async (m, { conn }) => {
+  let who = m.mentionedJid && m.mentionedJid[0]
+            ? m.mentionedJid[0]
+            : m.fromMe
+            ? conn.user.jid
+            : m.sender;
+  let uniqid = `${who.split`@`[0]}`;
+  let sessionPath = `./serbot/${uniqid}`;
+
+  try {
+    await fs.rm(sessionPath, { recursive: true, force: true });
+    await conn.sendMessage(m.chat, { text: '🚩 Sub-Bot eliminado correctamente.' }, { quoted: m });
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      await conn.sendMessage(m.chat, { text: "❌ No se encontró ninguna sesión activa de Sub-Bot." }, { quoted: m });
+    } else {
+      console.error("Error al eliminar la sesión del Sub-Bot:", err.message);
+      await m.react('✖️');
+    }
+  }
+};
+
+handler.tags = ['serbot'];
+handler.help = ['delsesion'];
+handler.command = /^(delsession|delsesion|eliminarsesion|borrarsesion|cerrarsesion)$/i;
+
+export default handler;
