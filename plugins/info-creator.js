fe85fa565dc5955d7710a88f6ab8226c5767a9f8
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2025-01-19 20:15:57 -0500

    Update info-creator.js

--- plugins/info-creator.js
+++ plugins/info-creator.js
@@ -24,7 +24,7 @@ let handler = async (m, { conn, usedPrefix, text, args, command }) => {
                 showAdAttribution: true,
                 title: 'һ᥆ᥣᥲ s᥆ᥡ ᑕrᥱᥲძ᥆r Darkcore ☁️',
                 body: 'Este es el contacto oficial de mi creador',
-                thumbnailUrl: 'https://i.ibb.co/44XMFDQ/file.jpg',
+                thumbnailUrl: '',
                 sourceUrl: 'https://darkcore-support.vercel.app/',
                 mediaType: 1,
                 renderLargerThumbnail: true
