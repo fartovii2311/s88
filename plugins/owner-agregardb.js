b761c6ac75e1ceb1b7eb5e8daedca57e31bf03b7
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2025-01-20 16:06:49 -0500

    Update owner-agregardb.js

--- plugins/owner-agregardb.js
+++ plugins/owner-agregardb.js
@@ -1,4 +1,7 @@
-import { loadDatabase, saveDatabase } from '../storage/databases/database.json';
+import fs from 'fs';
+import path from 'path';
+
+const databasePath = path.resolve('./storage/databases/database.json');
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
     let db = loadDatabase();
@@ -39,10 +42,21 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     await conn.reply(m.chat, replyText, m);
 };
 
+function loadDatabase() {
+    if (fs.existsSync(databasePath)) {
+        return JSON.parse(fs.readFileSync(databasePath, 'utf8'));
+    } else {
+        return {};
+    }
+}
+
+function saveDatabase(db) {
+    fs.writeFileSync(databasePath, JSON.stringify(db, null, 2), 'utf8');
+}
+
 handler.help = ['agregardata'];
 handler.tags = ['owner'];
 handler.command = /^agregardata$/i;
 handler.rowner = true;
 
 export default handler;
-
