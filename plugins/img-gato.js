7360af7bb314fdd35cb0cbb9b231d56b3990507b
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2025-01-20 18:31:46 -0500

    Update img-gato.js

--- plugins/img-gato.js
+++ plugins/img-gato.js
@@ -1,20 +1,34 @@
 import axios from 'axios';
 
-const handler = async (m, {conn, text, usedPrefix, command}) => {
+const handler = async (m, { conn, text, usedPrefix, command }) => {
     try {
         const response = await axios.get('https://some-random-api.com/animal/cat');
         const imageUrl = response.data.image;
 
-        await conn.sendFile(m.chat, imageUrl, 'gato.jpg', "🐱 *G A T O* 🐱", null, m, rcanal);
-
+        await conn.sendMessage(
+            m.chat, 
+            { 
+                image: { url: imageUrl },
+                caption: "🐱 *G A T O* 🐱\nDisfruta de esta imagen aleatoria de un gato.",
+                buttons: [
+                    { 
+                        buttonId: 'mas_gatos',
+                        buttonText: { displayText: 'Otro Gato 🐾' },
+                        type: 1,
+                    }
+                ],
+                headerType: 4
+            },
+            { quoted: m }
+        );
     } catch (error) {
         console.error('Error al obtener la imagen del gato:', error);
     }
 };
 
-handler.help = ['gato']
-handler.tags = ['img']
-handler.register = true
+handler.help = ['gato'];
+handler.tags = ['img'];
+handler.register = true;
 handler.command = /^(gato|cat|gatarandom)$/i;
 
 export default handler;
