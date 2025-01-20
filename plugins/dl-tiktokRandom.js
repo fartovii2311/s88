325c0ed2cb3abf2f41bbf29e4faff15b043fb202
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2025-01-20 13:58:46 -0500

    Update dl-tiktokRandom.js

--- plugins/dl-tiktokRandom.js
+++ plugins/dl-tiktokRandom.js
@@ -52,7 +52,6 @@ let handler = async (m, { conn, args, text, usedPrefix, command }) => {
 handler.help = ['tiktokrandom *<consulta>*'];
 handler.tags = ['dl'];
 handler.command = ['tiktokrandom','tkrandom'];
-handler.limit = true;
 handler.register = true;
 
 export default handler;
