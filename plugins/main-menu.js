e8bb892941dbd3da83d1df65a179330e599c205e
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2025-01-21 09:46:58 -0500

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -172,10 +172,10 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
  readmore: readMore
    }
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
-    let img = ''
+     let img = 'https://i.ibb.co/Y7mhFdf/file.jpg'
     await m.react('🍁')
-   await conn.reply(m.chat, text.trim(), { quoted: m, mentions: [m.sender], contextInfo: { mentionedJid: [m.sender] }, ...menu});
-
+   // await conn.sendMessage(m.chat, { video: { url: [pp, pp2, pp3, pp4, pp5, pp6, pp7, pp8, pp9, pp10, pp11, pp12, pp13, pp14, pp15].getRandom() }, gifPlayback: true, caption: text.trim(), mentions: [m.sender] }, { quoted: estilo })
+    await conn.sendFile(m.chat, text.trim(), m, null, menu)
   } catch (e) {
     conn.reply(m.chat, '❎ Lo sentimos, el menú tiene un error.', m)
     throw e
