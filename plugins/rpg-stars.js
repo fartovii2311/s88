54ee0de3aaf105ce5d063b88b7e00fe9c24748a8
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2025-01-21 14:57:11 -0500

    Update rpg-stars.js

--- plugins/rpg-stars.js
+++ plugins/rpg-stars.js
@@ -5,20 +5,20 @@ let handler = async (m, {conn, usedPrefix}) => {
     let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
     let user = global.db.data.users[who]
     let name = conn.getName(who)
-    if (!(who in global.db.data.users)) return conn.reply(m.chat, '🤍 El usuario no se encuentra en mi base de Datos.', m, rcanal).then(_ => m.react('✖️'))
+    if (!(who in global.db.data.users)) return conn.reply(m.chat, '🪙 El usuario no se encuentra en mi base de Datos.', m, rcanal).then(_ => m.react('✖️'))
     let img = await (await fetch(`https://i.ibb.co/JndpnfX/LynxAI.jpg`)).buffer()
-    let txt = ` –  *CORAZONES -  U S E R*\n\n`
+    let txt = ` –  *🪙 M O N E D A S -  U S E R*\n\n`
         txt += `✩ *Nombre* : ${user.name}\n`
-        txt += `✩ *Corazones* : ${toNum(user.corazones)} ( *${user.corazones}* )\n`
+        txt += `✩ *🪙 Monedas* : ${toNum(user.corazones)} ( *${user.corazones}* )\n`
         txt += `✩ *Bank* : ${toNum(user.bank)} ( *${user.bank}* )\n`
         txt += `✩ *XP* : ${toNum(user.exp)} ( *${user.exp}* )`
     let mentionedJid = [who]
         
 await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal)
 }
-handler.help = ['corazones']
+handler.help = ['monedas']
 handler.tags = ['rpg']
-handler.command = ['coins', 'wallet', 'cartera', 'corazones', 'stars', 'bal', 'balance']
+handler.command = ['coins', 'wallet', 'cartera', 'monedas', 'Monedas', 'bal', 'balance']
 handler.register = true 
 export default handler
 
