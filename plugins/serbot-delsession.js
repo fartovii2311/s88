2afbb9bcdbd8bfd1a8d022cfb739de5194c38bfa
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2025-01-21 10:06:03 -0500

    Update serbot-delsession.js

--- plugins/serbot-delsession.js
+++ plugins/serbot-delsession.js
@@ -1,25 +1,26 @@
-import { readdirSync, statSync, unlinkSync, existsSync, readFileSync, watch, rmSync, promises as fs} from "fs"
-import path, { join } from 'path'
+import { readdirSync, statSync, unlinkSync, existsSync, readFileSync, watch, rmSync, promises as fs } from "fs";
+import path, { join } from 'path';
 
-let handler  = async (m, { conn: parentw, usedPrefix, command}, args) => {
+let handler = async (m, { conn: parentw, usedPrefix, command }, args) => {
+  let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
+  let uniqid = `${who.split`@`[0]}`;
+  let userS = `${conn.getName(who)}`;
 
-let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-let uniqid = `${who.split`@`[0]}`
-let userS = `${conn.getName(who)}`
+  try {
+    await fs.rmdir("./LynxJadiBot/" + uniqid, { recursive: true, force: true });
+    await parentw.sendMessage(m.chat, { text: '🚩 Sub-Bot eliminado.' }, { quoted: m });
+  } catch (err) {
+    if (err.code === 'ENOENT' && err.path === `./LynxJadiBot/${uniqid}`) {
+      await parentw.sendMessage(m.chat, { text: "🌠 No cuentas con ninguna sesión de Sub-Bot." }, { quoted: m });
+    } else {
+      await m.react("❌");
+    }
+  }
+};
 
-try {
-await fs.rmdir("./LynxJadiBot/" + uniqid, { recursive: true, force: true })
-await parentw.sendMessage(m.chat, { text: '🚩 Sub-Bot eliminado.' }, { quoted: fkontak })
-} catch(err) {
-if (err.code === 'ENOENT' && err.path === `./LynxJadiBot/${uniqid}`) {
-await parentw.sendMessage(m.chat, { text: "🌠 No cuentas con ninguna sesión de Sub-Bot." }, { quoted: fkontak })
-} else {
-await m.react("x")
-}}}
-handler.tags = ['serbot']
-handler.help = ['delsession']
-handler.command = /^(deletesess?ion|eliminarsesion|borrarsesion|delsess?ion|cerrarsesion|delserbot|logout)$/i
-//handler.private = true
-handler.fail = null
+handler.tags = ['serbot'];
+handler.help = ['delsession'];
+handler.command = /^(deletesess?ion|eliminarsesion|borrarsesion|delsess?ion|cerrarsesion|delserbot|logout)$/i;
+handler.fail = null;
 
-export default handler
+export default handler;
