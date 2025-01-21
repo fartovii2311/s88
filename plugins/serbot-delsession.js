3fd646d043218ac5bc7cd10a1e5b951b9d26af66
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2025-01-21 10:04:30 -0500

    Update serbot-delsession.js

--- plugins/serbot-delsession.js
+++ plugins/serbot-delsession.js
@@ -8,7 +8,7 @@ let uniqid = `${who.split`@`[0]}`
 let userS = `${conn.getName(who)}`
 
 try {
-await fs.rmdir("./CrowJadiBot/" + uniqid, { recursive: true, force: true })
+await fs.rmdir("./LynxJadiBot/" + uniqid, { recursive: true, force: true })
 await parentw.sendMessage(m.chat, { text: '🚩 Sub-Bot eliminado.' }, { quoted: fkontak })
 } catch(err) {
 if (err.code === 'ENOENT' && err.path === `./LynxJadiBot/${uniqid}`) {
