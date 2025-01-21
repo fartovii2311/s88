82174033e77a0c9947aec008a834d5a1f3678e5a
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2025-01-21 15:12:25 -0500

    Update rpg-rueda-fortuna.js

--- plugins/rpg-rueda-fortuna.js
+++ plugins/rpg-rueda-fortuna.js
@@ -9,49 +9,44 @@ let handler = async (m, { conn, text, command }) => {
 
   if (cooldowns[m.sender] && Date.now() - cooldowns[m.sender] < tiempoEspera * 1000) {
     let tiempoRestante = segundosAHMS(Math.ceil((cooldowns[m.sender] + tiempoEspera * 1000 - Date.now()) / 1000))
-    return conn.reply(m.chat,`🎰 Espera *${tiempoRestante}* para girar la rueda nuevamente.`,m,rcanal)
+    return conn.reply(m.chat, `🎰 Espera *${tiempoRestante}* para girar la ruleta nuevamente.`, m, rcanal)
   }
 
   cooldowns[m.sender] = Date.now()
 
   let resultados = [
-    '🤍 100 corazones blancos', 
-    '🎁 1 Skin', 
-    '🤍 50 corazones blancos', 
-    '✨ 30 XP', 
-    '🚫 Nada',
-    '🌟 Premium por 1 hora'
+    '🪙 100 monedas',
+    '🪙 50 monedas',
+    '✨ 30 XP',
+    '✨ 50 XP',
+    '🚫 Nada'
   ]
   let resultado = resultados[Math.floor(Math.random() * resultados.length)]
 
   switch (resultado) {
-    case '🤍 100 corazones blancos':
-      users[senderId].corazones = users[senderId].corazones || 0
-      users[senderId].corazones += 100
-      return conn.reply(m.chat, `🎰 ¡Felicidades, ${senderName}! Has ganado *100 🤍 corazones blancos*.`, m,rcanal)
-    case '🎁 1 Skin':
-      users[senderId].skins = users[senderId].skins || []
-      users[senderId].skins.push('Skin Especial')
-      return conn.reply(m.chat, `🎰 ¡Felicidades, ${senderName}! Has ganado una *Skin Especial*!`, m,rcanal)
-    case '🤍 50 corazones blancos':
-      users[senderId].corazones = users[senderId].corazones || 0
-      users[senderId].corazones += 50
-      return conn.reply(m.chat, `🎰 ¡Felicidades, ${senderName}! Has ganado *50 🤍 corazones blancos*.`, m,rcanal)
+    case '🪙 100 monedas':
+      users[senderId].Monedas = users[senderId].Monedas || 0
+      users[senderId].Monedas += 100
+      return conn.reply(m.chat, `🎰 ¡Felicidades, ${senderName}! Has ganado *100 🪙 monedas*.`, m, rcanal)
+    case '🪙 50 monedas':
+      users[senderId].Monedas = users[senderId].Monedas || 0
+      users[senderId].Monedas += 50
+      return conn.reply(m.chat, `🎰 ¡Felicidades, ${senderName}! Has ganado *50 🪙 monedas*.`, m, rcanal)
     case '✨ 30 XP':
       users[senderId].xp = users[senderId].xp || 0
       users[senderId].xp += 30
-      return conn.reply(m.chat, `🎰 ¡Felicidades, ${senderName}! Has ganado *30 ✨ XP*.`, m,rcanal)
+      return conn.reply(m.chat, `🎰 ¡Felicidades, ${senderName}! Has ganado *30 ✨ XP*.`, m, rcanal)
+    case '✨ 50 XP':
+      users[senderId].xp = users[senderId].xp || 0
+      users[senderId].xp += 50
+      return conn.reply(m.chat, `🎰 ¡Felicidades, ${senderName}! Has ganado *50 ✨ XP*.`, m, rcanal)
     case '🚫 Nada':
-      return conn.reply(m.chat, `🎰 Lo siento, ${senderName}, no ganaste nada esta vez. ¡Intenta de nuevo más tarde!`, m,rcanal)
-    case '🌟 Premium por 1 hora':
-      users[senderId].premium = true
-      users[senderId].premiumTime = Date.now() + 1 * 60 * 60 * 1000  // 1 hora en milisegundos
-      return conn.reply(m.chat, `🎰 ¡Felicidades, ${senderName}! Has ganado *Premium por 1 hora*!`, m,rcanal)
+      return conn.reply(m.chat, `🎰 Lo siento, ${senderName}, no ganaste nada esta vez. ¡Intenta de nuevo más tarde!`, m, rcanal)
   }
 }
 
 handler.command = ['rueda', 'wheel']
-handler.tags = ['rpg'];
+handler.tags = ['rpg']
 handler.help = ['rueda']
 handler.register = true
 
