cac0b055dc26c60178867f907ba4b5de3d96cd9e
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2025-01-21 14:39:34 -0500

    Update rpg-lb.js

--- plugins/rpg-lb.js
+++ plugins/rpg-lb.js
@@ -9,10 +9,10 @@ let usersLim = sortedLim.map(enumGetKey)
 let usersLevel = sortedLevel.map(enumGetKey)
 let len = args[0] && args[0].length > 0 ? Math.min(5, Math.max(parseInt(args[0]), 5)) : Math.min(5, sortedExp.length)
 let text = `
-*\`Top ${len} Corazones 🤍\`*
+*\`Top ${len} Monedas 🪙\`*
 
 ✩ Tú eres el *${usersLim.indexOf(m.sender) + 1}* de *${usersLim.length}*
-✩ ${sortedLim.slice(0, len).map(({ jid, corazones }, i) => `${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *${corazones} 🤍*`).join`\n✩ `}
+✩ ${sortedLim.slice(0, len).map(({ jid, corazones }, i) => `${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *${corazones} 🪙*`).join`\n✩ `}
 
 
 *\`TOP ${len} XP 💫\`*
