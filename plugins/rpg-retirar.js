5d37ec3268086a046ab14e10889a6dc3bf884133
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2025-01-21 14:51:42 -0500

    Update rpg-retirar.js

--- plugins/rpg-retirar.js
+++ plugins/rpg-retirar.js
@@ -1,20 +1,20 @@
 let handler = async (m, { args }) => {
    let user = global.db.data.users[m.sender]
-   if (!args[0]) return m.reply('🤍 Ingresa la cantidad de *🤍 corazones* que deseas Retirar.')
+   if (!args[0]) return m.reply('🪙 Ingresa la cantidad de *🪙 monedas* que deseas retirar.')
    if (args[0] == 'all') {
       let count = parseInt(user.bank)
       user.bank -= count * 1
-      user.corazones += count * 1
-      await m.reply(`🤍 Retiraste *${count} 🤍 corazones* del Banco.`)
+      user.monedas += count * 1
+      await m.reply(`🪙 Retiraste *${count} 🪙 monedas* del Banco.`)
       return !0
    }
-   if (!Number(args[0])) return m.reply('🤍 La cantidad deve ser un Numero.')
+   if (!Number(args[0])) return m.reply('🪙 La cantidad debe ser un número.')
    let count = parseInt(args[0])
-   if (!user.bank) return m.reply('No tienes *🤍 corazones* en el Banco.')
-   if (user.bank < count) return m.reply(`Solo tienes *${user.bank} 🤍 corazones* en el Banco.`)
+   if (!user.bank) return m.reply('No tienes *🪙 monedas* en el Banco.')
+   if (user.bank < count) return m.reply(`Solo tienes *${user.bank} 🪙 monedas* en el Banco.`)
    user.bank -= count * 1
-   user.corazones += count * 1
-   await m.reply(`🤍 Retiraste *${count} 🤍 corazones* del Banco.`)
+   user.monedas += count * 1
+   await m.reply(`🪙 Retiraste *${count} 🪙 monedas* del Banco.`)
 }
 
 handler.help = ['retirar']
