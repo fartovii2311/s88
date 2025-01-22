144721e7dbe82acf489be70e292f6439e544b537
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2025-01-22 17:11:46 -0500

    Update dl-modapk.js

--- plugins/dl-modapk.js
+++ plugins/dl-modapk.js
@@ -1,8 +1,8 @@
 import gplay from 'google-play-scraper'
 import fetch from 'node-fetch'
 let handler = async (m, {conn, args, usedPrefix: prefix, command, text}) => {
-if (!args[0]) throw `*❌ Ingresa un link de la PlayStore. Ejemplo:*\n\n ${prefix + command} https://play.google.com/store/apps/details?id=com.facebook.lite`
-m.react(rwait)
+if (!args[0]) throw `*❌ Ingresa un link de la PlayStore. Ejemplo:*\n\n ${prefix + command} url`;
+m.react('')
 const url = `${args[0]}`;
 const packageName = url.split("=")[1];
 console.log(packageName);
@@ -11,7 +11,7 @@ const h = info.title
 console.log(h + `\n` + info.appId)
 let link = `https://d.apkpure.com/b/APK/${info.appId}?version=latest`
 conn.sendFile(m.chat, link, h + '.apk', ``, m, false, { mimetype: 'application/videos.android.package-archive', asDocument: true })
-m.react(done)
+m.react('')
 }
 handler.help = ['dlplaystore *<url>*'];
 handler.tags = ['dl'];
