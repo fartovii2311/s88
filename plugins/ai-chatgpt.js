9732a1a9ed3e37fbba83ebf031b4dbcad05707e4
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2025-01-22 11:35:01 -0500

    Update ai-chatgpt.js

--- plugins/ai-chatgpt.js
+++ plugins/ai-chatgpt.js
@@ -1,6 +1,7 @@
 import fetch from "node-fetch";
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
+  if (!text) throw m.reply(`El comando necesita una descripción para hablar.\n\n *✧ Ejemplo:*\n${usedPrefix + command} hola bot`);
   try {
     let response = await fetch(`https://delirius-apiofc.vercel.app/ia/chatgpt?q=${encodeURIComponent(text)}`);
     if (!response.ok) throw new Error(`Error en la solicitud: ${response.statusText}`);
