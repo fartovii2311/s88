1d23967abee691aaa343186513ef7f3ab1faae59
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2025-01-23 15:04:57 -0500

    Update owner-banuser.js

--- plugins/owner-banuser.js
+++ plugins/owner-banuser.js
@@ -7,12 +7,19 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     let user = global.db.data.users[who]
     if (!who) return m.reply(`🚩 Etiqueta a un usuario.`)
     let users = global.db.data.users
-    users[who].banned = true
-    conn.reply(m.chat, `🚩 @${who.split`@`[0]} ha sido baneado con exito, ya no podrá volver a usar mis comandos.`, m, { mentions: [who] })
+
+    if (command === 'mban') {
+        users[who].banned = true
+        conn.reply(m.chat, `🚩 @${who.split`@`[0]} ha sido baneado con éxito, ya no podrá volver a usar mis comandos.`, m, { mentions: [who] })
+    } else if (command === 'munban') {
+        if (!users[who].banned) return conn.reply(m.chat, `⚠️ El usuario @${who.split`@`[0]} no está baneado.`, m, { mentions: [who] })
+        users[who].banned = false
+        conn.reply(m.chat, `✅ @${who.split`@`[0]} ha sido desbaneado con éxito, ahora podrá usar mis comandos.`, m, { mentions: [who] })
+    }
 }
-handler.help = ['mban *@user*']
+handler.help = ['mban *@user*', 'munban *@user*']
 handler.tags = ['owner']
-handler.command = /^mban$/i
+handler.command = /^(mban|munban)$/i
 handler.rowner = true
 
 export default handler
