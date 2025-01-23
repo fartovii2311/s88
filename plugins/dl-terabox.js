c4a4d435724cd226d98b937adef226034e644891
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2025-01-22 19:46:46 -0500

    Update dl-terabox.js

--- plugins/dl-terabox.js
+++ plugins/dl-terabox.js
@@ -37,7 +37,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 handler.help = ["terabox *<url>*"];
 handler.tags = ["dl"];
 handler.command = ["terabox"];
-
+handler.Monedas = 1
 export default handler;
 
 async function terabox(url) {
