0a0bbc133f37954e168272a926fe9751a748dbce
deylinqff <maycraftminecraft4@gmail.com>
2025-01-22 21:32:10 -0600

    Actualizar tools-imagenAI.js

--- plugins/tools-imagenAI.js
+++ plugins/tools-imagenAI.js
@@ -1,20 +1,26 @@
 import fetch from 'node-fetch';
-const handler = async (m, {conn, text, usedPrefix, command}) => {
-  if (!text) throw `*🌸 Ingresa un texto para generar tu imagen a tu gusto*`;
+import Jimp from 'jimp';
+
+const handler = async (m, { conn, text }) => {
+  if (!text) throw '*🌸 Ingresa un texto para generar tu imagen a tu gusto*';
   m.react('🪷');
   await conn.reply(m.chat, '*🌺 Espere, Estamos Trabajando en su imagen*', m);
-  
+
   try {
-    
     const response = await fetch(`https://eliasar-yt-api.vercel.app/api/ai/text2img?prompt=${encodeURIComponent(text)}`);
-    
     if (!response.ok) throw new Error('Network response was not ok');
-   
+
     const buffer = await response.buffer();
-    // Mostramos un emoji de éxito
-    m.react('✔️');
-    
-    
+    const mainImage = await Jimp.read(buffer);
+    const watermark = await Jimp.read('/mnt/data/file-JgYMG1XBibWfXjyvceirRs');
+
+    watermark.resize(100, 100);
+    const x = mainImage.bitmap.width - watermark.bitmap.width - 10;
+    const y = mainImage.bitmap.height - watermark.bitmap.height - 10;
+    mainImage.composite(watermark, x, y, { mode: Jimp.BLEND_SOURCE_OVER, opacitySource: 0.8 });
+
+    const finalBuffer = await mainImage.getBufferAsync(Jimp.MIME_JPEG);
+
     const thumbBuffer = fs.readFileSync('https://files.catbox.moe/ohla62.png'); 
     await conn.sendFile(m.chat, buffer, 'imagen.jpg', '¡Aquí está tu imagen generada! 🎨', m, false, {
       thumbnail: thumbBuffer
@@ -24,9 +30,9 @@ const handler = async (m, {conn, text, usedPrefix, command}) => {
     throw `*🚨 Lo sentimos, ha ocurrido un error 😔*`;
   }
 }
-
 handler.tags = ['tools'];
 handler.help = ['genearimg'];
 handler.command = ['genearimg', 'imgg'];
 handler.Monedas = 1;
+
 export default handler;
