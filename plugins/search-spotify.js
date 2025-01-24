2d6a406d57a4048436272198dd2bb54c789fe88c
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2025-01-23 20:58:41 -0500

    Update search-spotify.js

--- plugins/search-spotify.js
+++ plugins/search-spotify.js
@@ -75,5 +75,6 @@ console.error(error)
 handler.help = ["spotifysearch *<text>*"]
 handler.tags = ["search"]
 handler.command = /^(spotifysearch)$/i
-handler.register = true
+handler.register = true;
+handler.Monedas = 5
 export default handler
