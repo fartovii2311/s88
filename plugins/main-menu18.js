3b9c9db762473c83fcbe1f8fc3eeca5bb87135cc
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2025-01-24 18:15:33 -0500

    Create main-menu18.js

---
+++ plugins/main-menu18.js
@@ -0,0 +1,25 @@
+let handler = async (m, { isPrems, conn }) => {
+
+let img = '' 
+let texto = `
+`
+const fkontak = {
+        "key": {
+    "participants":"0@s.whatsapp.net",
+                "remoteJid": "status@broadcast",
+                "fromMe": false,
+                "id": "Halo"
+        },
+        "message": {
+                "contactMessage": {
+                        "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
+                }
+        },
+        "participant": "0@s.whatsapp.net"
+}
+await conn.sendFile(m.chat, img, 'img.jpg', texto, fkontak)
+}
+handler.help = ['menuhot (menu +18)']
+handler.tags = ['crow']
+handler.command = ['menu18', 'menuhorny', 'menunsfw', 'menuhot'] 
+export default handler;
