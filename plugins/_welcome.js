4e90ed576ea893e6c347cce84035c7eadc7d3fc8
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2025-01-24 18:00:54 -0500

    Update _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -1,29 +1,24 @@
-import { WAMessageStubType } from '@whiskeysockets/baileys';
-import fetch from 'node-fetch';
+import {WAMessageStubType} from '@whiskeysockets/baileys'
+import fetch from 'node-fetch'
 
-export async function before(m, { conn, participants, groupMetadata }) {
-    if (!m.messageStubType || !m.isGroup) return !0;
-
-    let chat = global.db.data.chats[m.chat];
-    let user = global.db.data.users[m.sender];
-
+export async function before(m, {conn, participants, groupMetadata}) {
+  if (!m.messageStubType || !m.isGroup) return !0;
     if (!chat || !chat.bienvenida || !user?.registered) return;
+  let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://th.bing.com/th/id/R.3c44682163aece471be5e9be31853c5f?rik=ffeQ00G9XjrtnA&riu=http%3a%2f%2fcdn.wallpapersafari.com%2f3%2f96%2fzCEgo6.jpg&ehk=AG0SIiF60d%2fqhZysxXu70HHHGZOSdQ5xhUnW0SeytiI%3d&risl=&pid=ImgRaw&r=0')
+  let img = await (await fetch(${pp})).buffer()
+  let chat = global.db.data.chats[m.chat]
 
-    let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://th.bing.com/th/id/R.3c44682163aece471be5e9be31853c5f?rik=ffeQ00G9XjrtnA&riu=http%3a%2f%2fcdn.wallpapersafari.com%2f3%2f96%2fzCEgo6.jpg&ehk=AG0SIiF60d%2fqhZysxXu70HHHGZOSdQ5xhUnW0SeytiI%3d&risl=&pid=ImgRaw&r=0');
-    let img = await (await fetch(`${pp}`)).buffer();
+  if (chat.bienvenida && m.messageStubType == 27) {
+    let welcome = *⭒─ׄ─ׅ─ׄ─⭒ \ʙɪᴇɴᴠᴇɴɪᴅᴀ\ ⭒─ׄ─ׅ─ׄ─⭒*\n\n╭── ︿︿︿︿︿ *⭒   ⭒   ⭒   ⭒   ⭒   ⭒*\n┊:⁖֟⊱┈֟፝❥ *ᴡᴇʟᴄᴏᴍᴇ* :: @${m.messageStubParameters[0].split@[0]}\n┊:⁖֟⊱┈֟፝❥  ${groupMetadata.subject}\n╰─── ︶︶︶︶ ✰⃕  ⌇ *⭒ ⭒ ⭒*   ˚̩̥̩̥*̩̩͙✩\n\n> ✐ Puedes usar */help* para ver la lista de comandos.
+await conn.sendAi(m.chat, titulowm2, titu, welcome, img, img, canal, estilo)
+  }
 
-    if (chat.bienvenida && m.messageStubType == 27) {
-        let welcome = `*⭒─ׄ─ׅ─ׄ─⭒ \ʙɪᴇɴᴠᴇɴɪᴅᴀ\ ⭒─ׄ─ׅ─ׄ─⭒*\n\n╭── ︿︿︿︿︿ *⭒   ⭒   ⭒   ⭒   ⭒   ⭒*\n┊:⁖֟⊱┈֟፝❥ * ᴡᴇʟᴄᴏᴍᴇ * :: @${m.messageStubParameters[0].split('@')[0]} \n┊:⁖֟⊱┈֟፝❥  ${groupMetadata.subject} \n╰─── ︶︶︶︶ ✰⃕  ⌇ *⭒ ⭒ ⭒*   ˚̩̥̩̥*̩̩͙✩\n\n > ✐ Puedes usar */help* para ver la lista de comandos.`;
-        await conn.sendAi(m.chat, titulowm2, titu, welcome, img, img, canal, estilo);
-    }
+  if (chat.bienvenida && m.messageStubType == 28) {
+    let bye = *⭒─ׄ─ׅ─ׄ─⭒ \ᴀ ᴅ ɪ ᴏ ꜱ\ ⭒─ׄ─ׅ─ׄ─⭒*\n\n╭── ︿︿︿︿︿ *⭒   ⭒   ⭒   ⭒   ⭒   ⭒*\n┊:⁖֟⊱┈֟፝❥ *ʙ ʏ ᴇ* :: @${m.messageStubParameters[0].split@[0]}\n┊:⁖֟⊱┈֟፝❥  SE NOS FUE XD\n╰─── ︶︶︶︶ ✰⃕  ⌇ *⭒ ⭒ ⭒*   ˚̩̥̩̥*̩̩͙✩
+await conn.sendAi(m.chat, titulowm2, titu, bye, img, img, canal, estilo)
+  }
 
-    if (chat.bienvenida && m.messageStubType == 28) {
-        let bye = `*⭒─ׄ─ׅ─ׄ─⭒ \ᴀ ᴅ ɪ ᴏ ꜱ\ ⭒─ׄ─ׅ─ׄ─⭒*\n\n╭── ︿︿︿︿︿ *⭒   ⭒   ⭒   ⭒   ⭒   ⭒*\n┊:⁖֟⊱┈֟፝❥ * ʙ ʏ ᴇ * :: @${m.messageStubParameters[0].split('@')[0]} \n┊:⁖֟⊱┈֟፝❥  SE NOS FUE XD\n╰─── ︶︶︶︶ ✰⃕  ⌇ *⭒ ⭒ ⭒*   ˚̩̥̩̥*̩̩͙✩`;
-        await conn.sendAi(m.chat, titulowm2, titu, bye, img, img, canal, estilo);
-    }
-
-    if (chat.bienvenida && m.messageStubType == 32) {
-        let kick = `*⭒─ׄ─ׅ─ׄ─⭒ \ᴀ ᴅ ɪ ᴏ ꜱ\ ⭒─ׄ─ׅ─ׄ─⭒*\n\n╭── ︿︿︿︿︿ *⭒   ⭒   ⭒   ⭒   ⭒   ⭒*\n┊:⁖֟⊱┈֟፝❥ * ʙ ʏ ᴇ * :: @${m.messageStubParameters[0].split('@')[0]} \n┊:⁖֟⊱┈֟፝❥  SE NOS FUE XD\n╰─── ︶︶︶︶ ✰⃕  ⌇ *⭒ ⭒ ⭒*   ˚̩̥̩̥*̩̩͙✩`;
-        await conn.sendAi(m.chat, titulowm2, titu, kick, img, img, canal, estilo);
-    }
-}
+  if (chat.bienvenida && m.messageStubType == 32) {
+    let kick = *⭒─ׄ─ׅ─ׄ─⭒ \ᴀ ᴅ ɪ ᴏ ꜱ\ ⭒─ׄ─ׅ─ׄ─⭒*\n\n╭── ︿︿︿︿︿ *⭒   ⭒   ⭒   ⭒   ⭒   ⭒*\n┊:⁖֟⊱┈֟፝❥ *ʙ ʏ ᴇ* :: @${m.messageStubParameters[0].split@[0]}\n┊:⁖֟⊱┈֟፝❥  SE NOS FUE XD\n╰─── ︶︶︶︶ ✰⃕  ⌇ *⭒ ⭒ ⭒*   ˚̩̥̩̥*̩̩͙✩
+await conn.sendAi(m.chat, titulowm2, titu, kick, img, img, canal, estilo)
+}} 
