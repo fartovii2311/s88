edcc3b5cf9e0a20cf937750af22b3344576e0b38
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2025-01-25 00:51:52 -0500

    Update rpg-deschetar.js

--- plugins/rpg-deschetar.js
+++ plugins/rpg-deschetar.js
@@ -1,1 +1,45 @@
+import MessageType from '@whiskeysockets/baileys';
+
+let handler = async (m, { conn, text }) => {
+    let who;
+
+    if (m.isGroup) {
+        if (m.mentionedJid.length > 0) {
+            who = m.mentionedJid[0];
+        } else {
+            who = m.sender;
+        }
+    } else {
+        who = m.sender;
+    }
+
+    let users = global.db.data.users;
+
+    const isOwner = global.owner.some(([jid]) => m.sender.endsWith(jid));
+    if (!isOwner) throw '🚩 Solo los propietarios pueden usar este comando.';
+
+    if (text.startsWith('deschetar')) {
+        if (!users[who]) throw '🔮 El usuario no tiene datos para deschetar.';
+
+        users[who].Monedas = 0;
+        users[who].exp = 0;
+
+        await m.reply(
+            `🔮 *¡Usuario descheteado con éxito!*\n\n` +
+            `👤 Usuario: @${who.split`@`[0]}\n` +
+            `🪙 Monedas: *0*\n` +
+            `💡 Experiencia (XP): *0*`,
+            null,
+            { mentions: [who] }
+        );
+    }
+};
+
+handler.help = ['deschetar *@user*'];
+handler.tags = ['owner'];
+handler.command = ['deschetar'];
+handler.register = true;
+handler.rowner = true;
+
+export default handler;
 
