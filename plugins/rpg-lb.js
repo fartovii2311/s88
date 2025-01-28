0ffb8ac0859dd444c40c1037fbb7a7d94078aead
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2025-01-28 17:10:20 -0500

    Update rpg-lb.js

--- plugins/rpg-lb.js
+++ plugins/rpg-lb.js
@@ -1,53 +1,69 @@
 let handler = async (m, { conn, args, participants }) => {
-let users = Object.entries(global.db.data.users).map(([key, value]) => {
-return {...value, jid: key}})
-let sortedExp = users.map(toNumber('exp')).sort(sort('exp'))
-let sortedLim = users.map(toNumber('Monedas')).sort(sort('Monedas'))
-let sortedLevel = users.map(toNumber('level')).sort(sort('level'))
-let usersExp = sortedExp.map(enumGetKey)
-let usersLim = sortedLim.map(enumGetKey) 
-let usersLevel = sortedLevel.map(enumGetKey)
-let len = args[0] && args[0].length > 0 ? Math.min(5, Math.max(parseInt(args[0]), 5)) : Math.min(5, sortedExp.length)
-let text = `
-*\`Top ${len} Monedas 🪙\`*
+  let users = Object.entries(global.db.data.users).map(([key, value]) => {
+    return { ...value, jid: key };
+  });
+  
+  let sortedExp = users.map(toNumber('exp')).sort(sort('exp'));
+  let sortedLim = users.map(toNumber('Monedas')).sort(sort('Monedas'));
+  let sortedLevel = users.map(toNumber('level')).sort(sort('level'));
+  
+  let usersExp = sortedExp.map(enumGetKey);
+  let usersLim = sortedLim.map(enumGetKey);
+  let usersLevel = sortedLevel.map(enumGetKey);
+  
+  let len = args[0] && args[0].length > 0 ? Math.min(5, Math.max(parseInt(args[0]), 5)) : Math.min(5, sortedExp.length);
+  
+  let text = `
+  *🏆 Top ${len} Jugadores:*
 
-✩ Tú eres el *${usersLim.indexOf(m.sender) + 1}* de *${usersLim.length}*
-✩ ${sortedLim.slice(0, len).map(({ jid, Monedas }, i) => `${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *${Monedas} 🪙*`).join`\n✩ `}
+  *💰 Top ${len} Monedas 🪙*
 
+  ✩ *Tú eres el #${usersLim.indexOf(m.sender) + 1} de ${usersLim.length}*  
+  ✩ ${sortedLim.slice(0, len).map(({ jid, Monedas }, i) => {
+    return `${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} - *${Monedas} 🪙*`;
+  }).join('\n✩ ')}
+  
+  *💫 Top ${len} XP 💫*
 
-*\`TOP ${len} XP 💫\`*
+  ✩ *Tú eres el #${usersExp.indexOf(m.sender) + 1} de ${usersExp.length}*  
+  ✩ ${sortedExp.slice(0, len).map(({ jid, exp }, i) => {
+    return `${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} - *${exp} 💫*`;
+  }).join('\n✩ ')}
+  
+  *📈 Top ${len} Niveles 📈*
 
-✩ Tú eres el *${usersLim.indexOf(m.sender) + 1}* de *${usersLim.length}*
-✩ ${sortedExp.slice(0, len).map(({ jid, exp }, i) => `${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *${exp} 💫*`).join`\n✩ `}
+  ✩ *Tú eres el #${usersLevel.indexOf(m.sender) + 1} de ${usersLevel.length}*  
+  ✩ ${sortedLevel.slice(0, len).map(({ jid, level }, i) => {
+    return `${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} - *Nivel ${level} 📈*`;
+  }).join('\n✩ ')}
 
+  *✨ ¡Sigue luchando y sube de nivel! ✨*
+  `.trim();
 
-*\`Top ${len} Nivel 📈\`*
+  m.reply(text, null, { mentions: conn.parseMention(text) });
+};
 
-✩ Tú eres el *${usersLim.indexOf(m.sender) + 1}* de *${usersLim.length}*
-✩ ${sortedLevel.slice(0, len).map(({ jid, level }, i) => `${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *Nivel ${level} 📈*`).join`\n✩ `}`.trim()
-  m.reply(text, null, { mentions: conn.parseMention(text) })
-}
-handler.help = ['lb']
-handler.tags = ['rpg']
-handler.command = ['leaderboard', 'lb'] 
-handler.register = true 
-handler.fail = null
-handler.exp = 0
+handler.help = ['lb'];
+handler.tags = ['rpg'];
+handler.command = ['nivel', 'lb', 'tops']; 
+handler.register = true; 
+handler.fail = null;
+handler.exp = 0;
 
-export default handler
+export default handler;
 
 function sort(property, ascending = true) {
-  if (property) return (...args) => args[ascending & 1][property] - args[!ascending & 1][property]
-  else return (...args) => args[ascending & 1] - args[!ascending & 1]
+  if (property) return (...args) => args[ascending & 1][property] - args[!ascending & 1][property];
+  else return (...args) => args[ascending & 1] - args[!ascending & 1];
 }
 
 function toNumber(property, _default = 0) {
   if (property) return (a, i, b) => {
-    return {...b[i], [property]: a[property] === undefined ? _default : a[property]}
+    return { ...b[i], [property]: a[property] === undefined ? _default : a[property] };
   }
-  else return a => a === undefined ? _default : a
+  else return a => a === undefined ? _default : a;
 }
 
 function enumGetKey(a) {
-  return a.jid
+  return a.jid;
 }
