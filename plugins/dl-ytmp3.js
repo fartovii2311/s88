098f758496d5770169f524aff54778da35ea8782
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2025-01-28 15:51:37 -0500

    Update dl-ytmp3.js

--- plugins/dl-ytmp3.js
+++ plugins/dl-ytmp3.js
@@ -1,37 +1,32 @@
-/* 
-- Downloader Ytmp3 By Izumi-kzx
-- https://whatsapp.com/channel/0029VaJxgcB0bIdvuOwKTM2Y
-*/
 import fetch from 'node-fetch';
 
 const handler = async (m, { conn, text, usedPrefix, command }) => {
   await m.react('✖️');
-  if (!text) throw `Proporcióname el enlace de YouTube para que pueda ayudarte. 🎵`;
+  if (!text) throw `❌ Proporcióname el enlace de YouTube para que pueda ayudarte. 🎵`;
 
   await m.react('🕓');
 
   try {
-    const apiKey = 'xenzpedo';
-    const response = await fetch(`https://api.botcahx.eu.org/api/dowloader/yt?url=${encodeURIComponent(text)}&apikey=${apiKey}`);
+    const response = await fetch(`https://api.davidcyriltech.my.id/download/ytmp3?url=${encodeURIComponent(text)}`);
     const result = await response.json();
 
-    if (result.status && result.result && result.result.mp3) {
+    if (result.status === 200 && result.success && result.result && result.result.download_url) {
       await conn.sendMessage(
         m.chat,
         { 
-          audio: { url: result.result.mp3 }, 
-          mimetype: 'audio/mpeg' 
+          audio: { url: result.result.download_url }, 
+          mimetype: 'audio/mpeg', 
+          ptt: false 
         },
         { quoted: m }
       );
-
       await m.react('✅');
     } else {
-      throw new Error('Error: Unable to fetch audio');
+      throw new Error('No se pudo obtener el enlace de descarga');
     }
   } catch (error) {
     await m.react('❌');
-    m.reply(`❌ *Error:* ${error.message || 'An unknown error occurred'}`);
+    m.reply(`❌ *Error:* ${error.message || 'Ocurrió un error desconocido'}`);
   }
 };
 
@@ -39,48 +34,6 @@ handler.help = ['ytmp3 *<url>*'];
 handler.tags = ['dl'];
 handler.command = ['ytmp3'];
 handler.register = true;
-handler.Monedas = 3
+handler.Monedas = 3;
+
 export default handler;
-
-
-
-
-
-
-
-
-/* import fetch from "node-fetch";
-import yts from "yt-search";
-
-let handler = async (m, { conn, text }) => {
-  if (!text) {
-    return m.reply("*❀ Ingresa el texto de lo que quieres buscar*");
-  }
-
-  let ytres = await yts(text);
-  let video = ytres.videos[0];
-
-  if (!video) {
-    return m.reply("*❀ Video no encontrado*");
-  }
-
-  let { url } = video;
-
-  await m.react('🕓');
-
-  try {
-    let api = await fetch(`https://api.vreden.web.id/api/ytplaymp3?query=${url}`);
-    let json = await api.json();
-    let { download } = json.result;
-
-    await conn.sendMessage(m.chat, { audio: { url: download.url }, mimetype: "audio/mpeg" }, { quoted: m });
-    await m.react('✅');
-  } catch (error) {
-    console.error(error);
-    await m.react('✖️');
-  }
-};
-
-handler.command = /^(ytmp3)$/i;
-
-export default handler; */
