a1058ef4b12c6fe2bccdca7b2f4181a3cff1fe5f
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2025-01-28 17:35:49 -0500

    Update _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -3,77 +3,46 @@
 - Welcome con imagen Card
 */
 import { WAMessageStubType } from '@whiskeysockets/baileys';
-import fetch from 'node-fetch';
-import canvafy from 'canvafy';
 
 export async function before(m, { conn, participants, groupMetadata }) {
   if (!m.messageStubType || !m.isGroup) return !0;
 
   let chat = global.db.data.chats[m.chat];
-  let wel = 'ＷＥＬＣＯＭＥ － ＵＳＥＲ'
-  let bye = 'L Y N X － ＵＳＥＲ'
+  let wel = 'ＷＥＬＣＯＭＥ － ＵＳＥＲ';
+  let bye = 'L Y N X － ＵＳＥＲ';
   let web = '';
   let webb = '';
   let who = m.messageStubParameters[0] + '@s.whatsapp.net';
   let user = global.db.data.users[who];
   let userName = user ? user.name : await conn.getName(who);
 
-  const getUserAvatar = async () => {
-    try {
-      return await conn.profilePictureUrl(m.messageStubParameters[0], 'image');
-    } catch (err) {
-      return 'https://i.ibb.co/cFzgdNw/file.jpg';
-    }
-  };
-
-  const generateImage = async (title, description) => {
-    const userAvatar = await getUserAvatar();
-    const img = await new canvafy.WelcomeLeave()
-      .setAvatar(userAvatar)
-      .setBackground(
-        'image',
-        'https://imgs.search.brave.com/2b05LaxZ830ADvTmXuuUk7KWp6tJ4KbUAxwBAwreUD4/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93YWxs/cGFwZXJzLmNvbS9p/bWFnZXMvaGQvbmFy/dXRvLXV6dW1ha2kt/NGstemlqb3Q2YXNu/ZXM2ZDc3MC5qcGc'
-      )
-      .setTitle(title)
-      .setDescription(description)
-      .setBorder('#2a2e35')
-      .setAvatarBorder('#2a2e35')
-      .setOverlayOpacity(0.3)
-      .build();
-
-    return img;
-  };
-
   if (chat.welcome && m.messageStubType == 27) {
     let bienvenida = `❀ *Se unió* al grupo *${groupMetadata.subject.trim()}*\n    ✰ @${m.messageStubParameters[0].split`@`[0]} \n\n    Ꮚ⁠˘⁠ ⁠ꈊ⁠ ⁠˘⁠ ⁠Ꮚ ¡Bienvenido! ¡Esperamos que tengas un excelente día!\n\n> ✐ No olvides usar *#help* si necesitas algo.\n> 🜸 ¡Disfruta de tu tiempo con nosotros!`;
 
-    let img = await generateImage(
-      '¡BIENVENIDO!',
-      `¡Hola Bienvenido al grupo!`
-    );
-
-    await conn.sendAi(m.chat, 'ＷＥＬＣＯＭＥ － ＵＳＥＲ', dev, bienvenida, img, img, web, null);
+    // Enviar mensaje de bienvenida
+    await conn.sendMessage(m.chat, {
+      text: bienvenida,
+      mentions: [m.messageStubParameters[0] + '@s.whatsapp.net']
+    }, { quoted: m });
   }
 
   if (chat.welcome && m.messageStubType == 28) {
     let bye = `❀ *Se salió* del grupo  *${groupMetadata.subject.trim()}*\n    ✰ @${m.messageStubParameters[0].split`@`[0]}\n\n    Ꮚ⁠˘⁠ ⁠ꈊ⁠ ⁠˘⁠ ⁠Ꮚ ¡Nos vemos pronto! ¡Que tengas un buen día!\n\n> ✐ No olvides usar *#help* si necesitas algo.\n> 🜸 Adiós...`;
 
-    let img = await generateImage(
-      '¡ADIOS!',
-      `¡Hasta pronto Usuario!`
-    );
-
-    await conn.sendAi(m.chat, ' L Y N X － ＵＳＥＲ', dev, bye, img, img, webb, null);
+    // Enviar mensaje cuando alguien sale
+    await conn.sendMessage(m.chat, {
+      text: bye,
+      mentions: [m.messageStubParameters[0] + '@s.whatsapp.net']
+    }, { quoted: m });
   }
 
   if (chat.welcome && m.messageStubType == 32) {
-    let kick = `❀ *Se salió* del grupo  *${groupMetadata.subject.trim()}*\n    ✰ @${m.messageStubParameters[0].split`@`[0]}\n\n    Ꮚ⁠˘⁠ ⁠ꈊ⁠ ⁠˘⁠ ⁠Ꮚ ¡Nos vemos pronto! ¡Que tengas un buen día!\n\n> ✐ No olvides usar *#help* si necesitas algo.\n> 🜸 Adiós...`;
+    let kick = `❀ *Se eliminó* del grupo  *${groupMetadata.subject.trim()}*\n    ✰ @${m.messageStubParameters[0].split`@`[0]}\n\n    Ꮚ⁠˘⁠ ⁠ꈊ⁠ ⁠˘⁠ ⁠Ꮚ ¡Nos vemos pronto! ¡Que tengas un buen día!\n\n> ✐ No olvides usar *#help* si necesitas algo.\n> 🜸 Adiós...`;
 
-    let img = await generateImage(
-      '¡ADIOS!',
-      `¡Hasta pronto Usuario!`
-    );
-
-    await conn.sendAi(m.chat, 'L Y N X － ＵＳＥＲ', dev, kick, img, img, web, null);
+    // Enviar mensaje cuando alguien es eliminado
+    await conn.sendMessage(m.chat, {
+      text: kick,
+      mentions: [m.messageStubParameters[0] + '@s.whatsapp.net']
+    }, { quoted: m });
   }
 }
