b2858699f7f7865171e58483d1fca1be422c50c5
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2025-01-28 21:48:18 -0500

    Update _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -12,7 +12,7 @@ export async function before(m, { conn, participants, groupMetadata }) {
   let who = m.messageStubParameters[0]
   let taguser = `@${who.split('@')[0]}`
   let chat = global.db.data.chats[m.chat]
-  let defaultImage = '';
+  let defaultImage = 'https://i.ibb.co/Y7mhFdf/file.jpg';
 
   if (chat.welcome) {
     let img;
