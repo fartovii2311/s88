08d1f6abf16f7e1b0f14e82a47141d291a74bf0e
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2025-02-02 16:28:18 -0500

    Update dl-mediafire.js

--- plugins/dl-mediafire.js
+++ plugins/dl-mediafire.js
@@ -3,73 +3,52 @@ import cheerio from 'cheerio';
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
   if (!args[0]) {
-    return conn.reply(m.chat, `[ ᰔᩚ ] Ingresa una URL válida de *Mediafire*.`, m,rcanal);
+    return conn.reply(m.chat, `[ ᰔᩚ ] Ingresa una URL válida de *Mediafire*.`, m);
   }
   
   await m.react('🕓');
   
   let url = args[0];
   if (!url.includes('mediafire.com')) {
-    return conn.reply(m.chat,`El enlace proporcionado no parece ser de MediaFire.`,m,rcanal);
+    return conn.reply(m.chat, `El enlace proporcionado no parece ser de MediaFire.`, m);
   }
 
   try {
-    const { name, size, date, mime, link } = await mediafireDl(url);
+    const apiUrl = `https://api.siputzx.my.id/api/d/mediafire?url=${encodeURIComponent(url)}`;
+    const response = await axios.get(apiUrl);
     
-    let text = '`乂  M E D I A F I R E\n\n`'
-     text += `» *Título:* ${name}\n`
-     text += `» *Tamaño:* ${size}\n`
-     text += `» *Nime:* ${mime}\n\n`
-     text += `> ${namebot}`
+    if (!response.data.status || !response.data.data) {
+      throw new Error('No se pudo obtener la información del archivo.');
+    }
 
-    await conn.reply(m.chat,text,m,rcanal);
+    const { fileName, downloadLink, fileSize, meta } = response.data.data;
+    
+    let text = '`乂  M E D I A F I R E`
 
-    const response = await axios.get(link, { responseType: 'arraybuffer' });
-    const fileBuffer = response.data;
+';
+    text += `» *Título:* ${fileName}\n`;
+    text += `» *Tamaño:* ${fileSize}\n`;
+    text += `» *Enlace:* ${downloadLink}\n`;
 
+    await conn.reply(m.chat, text, m);
+
+    const fileBuffer = (await axios.get(downloadLink, { responseType: 'arraybuffer' })).data;
     await conn.sendMessage(
       m.chat,
-      { document: fileBuffer, fileName: name, mimetype: mime },
+      { document: fileBuffer, fileName: fileName, mimetype: 'application/octet-stream' },
       { quoted: m }
     );
-   await m.react('✅');
+    
+    await m.react('✅');
   } catch (error) {
     console.error(error);
     await m.react('❌');
   }
 };
 
-async function mediafireDl(url) {
-  const res = await axios.get(
-    `https://www-mediafire-com.translate.goog/${url.replace(
-      'https://www.mediafire.com/',
-      ''
-    )}?_x_tr_sl=en&_x_tr_tl=fr&_x_tr_hl=en&_x_tr_pto=wapp`
-  );
-  const $ = cheerio.load(res.data);
-  const link = $('#downloadButton').attr('href');
-  const name = $('body > main > div.content > div.center > div > div.dl-btn-cont > div.dl-btn-labelWrap > div.promoDownloadName.notranslate > div')
-    .attr('title')
-    .replaceAll(' ', '')
-    .replaceAll('\n', '');
-  const date = $('body > main > div.content > div.center > div > div.dl-info > ul > li:nth-child(2) > span').text();
-  const size = $('#downloadButton')
-    .text()
-    .replace('Download', '')
-    .replace('(', '')
-    .replace(')', '')
-    .replace('\n', '')
-    .replace('\n', '')
-    .replace('                         ', '')
-    .replaceAll(' ', '');
-  let mime = '';
-  let rese = await axios.head(link);
-  mime = rese.headers['content-type'];
-  return { name, size, date, mime, link };
-}
 handler.help = ['mediafire *<url>*'];
 handler.tags = ['dl'];
 handler.command = ['mediafire'];
-handler.register = true 
-handler.Monedas = 1
+handler.register = true;
+handler.Monedas = 1;
 export default handler;
