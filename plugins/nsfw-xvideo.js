a3a10b5e8f894847cf47450c952f1163e16d5a2a
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2025-02-01 20:14:38 -0500

    Update nsfw-xvideo.js

--- plugins/nsfw-xvideo.js
+++ plugins/nsfw-xvideo.js
@@ -1,6 +1,4 @@
 import fetch from 'node-fetch';
-import fs from 'fs';
-import { Document, Packer, Paragraph, TextRun } from 'docx';
 
 const handler = async (m, { conn, text }) => {
   if (!global.db.data.chats[m.chat].nsfw) {
@@ -24,56 +22,13 @@ const handler = async (m, { conn, text }) => {
         const videoTitle = 'Desconocido';
         const videoImage = videoData.ThumbUrl || '';
 
-        // Verifica el tamaño del video (puedes hacer esto con la respuesta del servidor si proporciona información sobre el tamaño)
-        const videoResponse = await fetch(videoUrl);
-        const videoBuffer = await videoResponse.buffer();
-        const videoSize = videoBuffer.length;
-
-        // Limite de tamaño en bytes (ejemplo: 5MB)
-        const maxSize = 5 * 1024 * 1024;
-
-        if (videoSize > maxSize) {
-          // Crear un archivo .docx
-          const doc = new Document({
-            sections: [
-              {
-                properties: {},
-                children: [
-                  new Paragraph({
-                    children: [
-                      new TextRun(`🎥 *Título:* ${videoTitle}\n`),
-                      new TextRun(`⏱️ *Duración:* Desconocida\n`),
-                      new TextRun(`🔗 *Enlace de Video:* ${videoUrl}\n`),
-                    ],
-                  }),
-                ],
-              },
-            ],
-          });
-
-          const docBuffer = await Packer.toBuffer(doc);
-          const docPath = './videoDetails.docx';
-          fs.writeFileSync(docPath, docBuffer);
-
-          // Enviar el archivo .docx
-          await conn.sendMessage(m.chat, {
-            document: fs.createReadStream(docPath),
-            fileName: 'videoDetails.docx',
-            mimetype: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
-            caption: `El archivo de video es demasiado grande, se ha enviado como un archivo de detalles.`,
-          }, { quoted: m });
-
-          fs.unlinkSync(docPath); // Eliminar el archivo temporal
-        } else {
-          // Si el tamaño es menor al límite, enviar el video directamente
-          await conn.sendMessage(m.chat, {
-            video: { url: videoUrl },
-            caption: `🎥 *Título:* ${videoTitle}\n⏱️ *Duración:* Desconocida`,
-            mimetype: 'video/mp4',
-            fileName: `${videoTitle}.mp4`,
-            thumbnail: { url: videoImage },
-          }, { quoted: m });
-        }
+        await conn.sendMessage(m.chat, {
+          video: { url: videoUrl },
+          caption: `🎥 *Título:* ${videoTitle}\n⏱️ *Duración:* Desconocida`,
+          mimetype: 'video/mp4',
+          fileName: `${videoTitle}.mp4`,
+          thumbnail: { url: videoImage },
+        }, { quoted: m });
 
         await m.react('✅');
       } else {
