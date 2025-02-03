f00463c4588dc2f1eb1081eee393447ff8128679
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2025-02-03 02:50:28 -0500

    Update rpg-chetar.js

--- plugins/rpg-chetar.js
+++ plugins/rpg-chetar.js
@@ -1,6 +1,6 @@
 import MessageType from '@whiskeysockets/baileys';
 
-let handler = async (m, { conn, text, isOwner}) => {
+let handler = async (m, { conn, text}) => {
     let who;
 
     // Verificar si es un grupo y si se menciona a un usuario
