1806b464b5254c3162f857f16ec171c011ac3e00
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2025-02-03 02:53:18 -0500

    Update owner-getdb.js

--- plugins/owner-getdb.js
+++ plugins/owner-getdb.js
@@ -1,7 +1,7 @@
 import fs from 'fs'
 let handler = async (m, { conn, text }) => {
     await m.react('🕓')
-    let dl_url = await fs.readFileSync('./storage/databases/database.json')
+    let dl_url = await fs.readFileSync('./storage/data/database.json')
     await conn.sendMessage(m.chat, { document: dl_url, mimetype: 'application/json', fileName: 'database.json' }, { quoted: m })
     await m.react('✅')
 }
