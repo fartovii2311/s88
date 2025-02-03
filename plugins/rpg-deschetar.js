ed5d8128aca9608da5ea3eb7f56995f778ee362b
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2025-02-03 02:25:18 -0500

    Update rpg-deschetar.js

--- plugins/rpg-deschetar.js
+++ plugins/rpg-deschetar.js
@@ -3,45 +3,52 @@ import MessageType from '@whiskeysockets/baileys';
 let handler = async (m, { conn, text }) => {
     let who;
 
-    // Verifica si es un grupo y se menciona a un usuario
     if (m.isGroup) {
         if (m.mentionedJid.length > 0) {
-            who = m.mentionedJid[0]; // Tomamos la primera mención
+            who = m.mentionedJid[0];
+        } else if (text) {
+            who = text.trim();
+            if (!who.endsWith('@s.whatsapp.net')) {
+                who = `${who}@s.whatsapp.net`;
+            }
         } else {
-            who = m.sender; // Si no se menciona a nadie, tomamos al remitente
+            who = m.sender;
         }
     } else {
-        who = m.sender; // Si no es un grupo, tomamos al remitente
+        if (text) {
+            who = text.trim();
+            if (!who.endsWith('@s.whatsapp.net')) {
+                who = `${who}@s.whatsapp.net`;
+            }
+        } else {
+            who = m.sender;
+        }
     }
 
     let users = global.db.data.users;
 
-    // Verificar si el remitente es propietario
     const isOwner = global.owner.some(([jid]) => m.sender.endsWith(jid));
     if (!isOwner) throw '🚩 Solo los propietarios pueden usar este comando.';
 
-    // Verificar si el perfil del usuario existe
     if (!users[who]) throw '🔮 El usuario no tiene datos para deschetar.';
 
-    // Restablecer todo (Monedas y XP) del usuario
-    users[who].Monedas = 0;  // Restauramos las monedas
-    users[who].exp = 0;      // Restauramos la experiencia (XP)
+    users[who].Monedas = 0;
+    users[who].exp = 0;
 
-    // Responder al propietario, mencionando al usuario
     await m.reply(
         `🔮 *¡Usuario descheteado con éxito!*\n\n` +
         `👤 Usuario: @${who.split`@`[0]}\n` +
         `🪙 Monedas: *0*\n` +
         `💡 Experiencia (XP): *0*`,
         null,
-        { mentions: [who] } // Esto menciona al usuario
+        { mentions: [who] }
     );
 };
 
-handler.help = ['deschetar *@user*'];
+handler.help = ['deschetar *@user*', 'deschetar *numero*'];
 handler.tags = ['owner'];
 handler.command = ['deschetar'];
 handler.register = true;
-handler.rowner = true; // Solo propietarios pueden usarlo
+handler.rowner = true;
 
 export default handler;
