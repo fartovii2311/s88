85f771ec9313f838207bc6a2e1ad59e5fa3d82b4
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2025-02-04 03:10:38 -0500

    Update _autolevenlup.js

--- plugins/_autolevenlup.js
+++ plugins/_autolevenlup.js
@@ -1,1 +1,27 @@
+import { canLevelUp, xpRange } from '../lib/levelling.js'
+import { levelup } from '../lib/canvas.js'
 
+export function before(m, { conn }) {
+    let user = global.db.data.users[m.sender]
+    let chat = global.db.data.chats[m.chat]
+    
+    if (!chat.autolevelup) return !0
+
+    let before = user.level * 1
+    while (canLevelUp(user.level, user.exp, global.multiplier)) user.level++
+
+    if (before !== user.level) {
+        const message = `
+🌟 *¡Felicidades! Has subido de nivel* 🌟
+
+🔥 *Nuevo Nivel:* ${user.level}
+💥 *Nivel Anterior:* ${before}
+🎯 *Rango Actual:* ${user.role}
+🕰️ *Fecha:* ${new Date().toLocaleString('id-ID')}
+        
+✨ ¡Sigue así! ¡Tu esfuerzo te llevará lejos! ✨
+        `.trim();
+
+        conn.reply(m.chat, message, m);
+    }
+}
