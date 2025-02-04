af78e95d097f791bb075d24d0e2ba6e09994fe51
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2025-02-04 18:38:55 -0500

    Update search-tiktok.js

--- plugins/search-tiktok.js
+++ plugins/search-tiktok.js
@@ -49,7 +49,7 @@ footer: proto.Message.InteractiveMessage.Footer.create({ text: '🔎 Tiktok - Bu
 header: proto.Message.InteractiveMessage.Header.create({ hasMediaAttachment: false }),
 carouselMessage: proto.Message.InteractiveMessage.CarouselMessage.fromObject({ cards: [...results] })})}}
 }, { quoted: message })
-await message.react('')
+await message.react('✅')
 await conn.relayMessage(message.chat, responseMessage.message, { messageId: responseMessage.key.id })
 } catch (error) {
 await conn.reply(message.chat, error.toString(), message)
