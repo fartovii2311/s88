be4df882e76e5da7081aa7dfe4f20a7e1564a786
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2025-02-04 18:25:58 -0500

    Update search-tiktok.js

--- plugins/search-tiktok.js
+++ plugins/search-tiktok.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { generateWAMessage, proto } from '@whiskeysockets/baileys';
+const { generateWAMessageContent, generateWAMessageFromContent, proto } = (await import('@whiskeysockets/baileys')).default
 
 let handler = async (message, { conn, text, usedPrefix, command }) => {
   if (!text) {
