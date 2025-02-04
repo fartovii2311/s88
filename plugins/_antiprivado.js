7a6ba673092a01c416c10feeb601a84503624db8
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2025-02-04 10:05:40 -0500

    Update _antiprivado.js

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -1,13 +1,29 @@
-export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner}) {
+export async function before(m, { conn, isAdmin, isBotAdmin, isOwner, isROwner, usedPrefix }) {
     if (m.isBaileys && m.fromMe) return !0;
     if (m.isGroup) return !1;
     if (!m.message) return !0;
-    if (m.text.includes('PIEDRA') || m.text.includes('PAPEL') || m.text.includes('TIJERA') || m.text.includes('serbot') || m.text.includes('.jadibot') || m.text.includes('.code')) return !0; // Agregado .code aquí
+
+    const prohibidas = ['PIEDRA', 'PAPEL', 'TIJERA'];
+
+    const permitidas = [
+        `${usedPrefix}serbot`, 
+        `${usedPrefix}jadibot`, 
+        `${usedPrefix}code`, 
+        `${usedPrefix}delsession`
+    ];
+
+    if (prohibidas.some(word => m.text.includes(word)) && !permitidas.some(cmd => m.text.includes(cmd))) {
+        return !0;
+    }
+
     const chat = global.db.data.chats[m.chat];
     const bot = global.db.data.settings[this.user.jid] || {};
+
     if (bot.antiPrivate && !isOwner && !isROwner) {
-      await m.reply(`> "⭐ Hola @${m.sender.split`@`[0]}, Lo Siento No Esta 📌Permitido Escribirme Al Privado ⚠️ Por Lo Cual Seras Bloqueado/A\n\n> *⭐ Wiii \n\n\n _Ayudame a cumplir mi meta\n Canal\nhttps://whatsapp.com/channel/0029Vaxk8vvEFeXdzPKY8f3F_`, false, {mentions: [m.sender]});
-      await this.updateBlockStatus(m.chat, 'block');
+        await m.reply(`> "⭐ Hola @${m.sender.split`@`[0]}, Lo Siento No Esta 📌Permitido Escribirme Al Privado ⚠️ Por Lo Cual Seras Bloqueado/A\n\n> *⭐ Wiii \n\n\n _Ayudame a cumplir mi meta_\n Canal\nhttps://whatsapp.com/channel/0029Vaxk8vvEFeXdzPKY8f3F_`, false, { mentions: [m.sender] });
+
+        await this.updateBlockStatus(m.chat, 'block');
     }
+
     return !1;
 }
