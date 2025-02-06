af401085969711a4be226759f9aa94449ccf9efb
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2025-02-06 10:26:52 -0500

    Update config.js

--- config.js
+++ config.js
@@ -24,7 +24,56 @@ global.prems = []
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
+global.keysZens = ['LuOlangNgentot', 'c2459db922', '37CC845916', '6fb0eff124', 'hdiiofficial', 'fiktod', 'BF39D349845E', '675e34de8a', '0b917b905e6f']
+global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
+global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
+global.keysxteam = keysxteammm[Math.floor(keysxteammm.length * Math.random())]
+global.keysneoxrrr = ['5VC9rvNx', 'cfALv5']
+global.keysneoxr = keysneoxrrr[Math.floor(keysneoxrrr.length * Math.random())]
+global.lolkeysapi = 'GataDiosV3'
+global.itsrose = ['4b146102c4d500809da9d1ff']
+global.baileys = '@whiskeysockets/baileys'
+global.apis = 'https://delirius-apiofc.vercel.app'
+
 global.jadi = 'LynxJadiBot'
+global.gataJadibts = true
+
+// ES ➜ Consigue Apikey en https://platform.openai.com/account/api-keys
+global.openai_key = 'sk-0'
+
+// ES ➜ Consigue tu ID de organizacion en: https://platform.openai.com/account/org-settings
+global.openai_org_id = 'org-3'
+
+global.APIs = { 
+lolhuman: { url: 'https://api.lolhuman.xyz/api/', key: lolkeysapi },
+neoxr: { url: 'https://api.neoxr.eu/api/', key: null },
+skizo: { url: 'https://skizo.tech/api/', key: 'GataDios' },
+aemt: { url: 'https://widipe.com/', key: null },
+alyachan: { url: 'https://api.alyachan.dev/api/', key: 'syah11' }, //muzan23 DitzOfc
+zahwazein: { url: 'https://api.zahwazein.xyz', key: null },
+akuari: { url: 'https://apimu.my.id', key: null },
+apimu: { url: 'https://api.xteam.xyz', key: null },
+fgmods: { url: 'https://api-fgmods.ddns.net', key: null },
+botcahx: { url: 'https://api.botcahx.biz.id', key: null },
+ibeng: { url: 'https://api.ibeng.tech/docs', key: null },
+itsrose: { url: 'https://api.itsrose.site', key: null },
+popcat: { url: 'https://api.popcat.xyz', key: null },
+xcoders: { url: 'https://api-xcoders.site', key: 'Frieren' }
+}
+
+global.APIKeys = { 
+  'https://api.xteam.xyz': `${keysxteam}`,
+  'https://api.lolhuman.xyz': `${lolkeysapi}`,
+  'https://api.neoxr.my.id': `${keysneoxr}`,	
+  'https://violetics.pw': 'beta',
+  'https://api.zahwazein.xyz': `${keysxxx}`,
+  'https://api-fgmods.ddns.net': 'fg-dylux',
+  'https://api.botcahx.biz.id': 'Admin',
+  'https://api.ibeng.tech/docs': 'tamvan',
+  'https://api.itsrose.site': 'Rs-Zeltoria',
+  'https://api-xcoders.site': 'Frieren'
+}
+
 global.packsticker = `【 ✫ 𝚃𝙴𝙰𝙼  乂 𝙳𝙰𝚁𝙺 - 𝙾𝙵𝙸𝙲𝙸𝙰𝙻 ✫ 】` //stiker
 global.author = '➼ 𝑳𝒚𝒏𝒙 - 𝑪𝒉𝒂𝒏𝒏𝒆𝒍' //rcanal 
 global.packname = '【 ✫ 𝚃𝙴𝙰𝙼  乂 𝙳𝙰𝚁𝙺 ✫ 】' //stiker
