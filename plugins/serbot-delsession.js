c539f778ac74b1d45570e509550714fc1b3a1a59
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2025-02-06 01:24:22 -0500

    Update serbot-delsession.js

--- plugins/serbot-delsession.js
+++ plugins/serbot-delsession.js
@@ -1,27 +1,25 @@
-import { existsSync } from "fs";
-import { rm } from "fs/promises";
+import { readdirSync, statSync, unlinkSync, existsSync, readFileSync, watch, rmSync, promises as fs} from "fs"
+import path, { join } from 'path'
 
-let handler = async (m, { conn: parentw }) => {
-  let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
-  let uniqid = `${who.split`@`[0]}`;
-  let folderPath = `./LynxJadiBot/${uniqid}`;
+let handler  = async (m, { conn: parentw, usedPrefix, command}, args) => {
 
-  try {
-    if (!existsSync(folderPath)) {
-      await parentw.sendMessage(m.chat, { text: "🌠 No cuentas con ninguna sesión de Sub-Bot." }, { quoted: m });
-      return;
-    }
+let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+let uniqid = `${who.split`@`[0]}`
+let userS = `${conn.getName(who)}`
 
-    await rm(folderPath, { recursive: true, force: true });
-    await parentw.sendMessage(m.chat, { text: "🚩 Sub-Bot eliminado." }, { quoted: m });
-  } catch (err) {
-    console.error("error", err);
-  }
-};
+try {
+await fs.rmdir("./LynxJadiBot/" + uniqid, { recursive: true, force: true })
+await parentw.sendMessage(m.chat, { text: '🚩 Sub-Bot eliminado.' }, { quoted: fkontak })
+} catch(err) {
+if (err.code === 'ENOENT' && err.path === `./IanJadiBot/${uniqid}`) {
+await parentw.sendMessage(m.chat, { text: "No cuentas con ninguna sesión de Sub-Bot." }, { quoted: fkontak })
+} else {
+await m.react(error)
+}}}
+handler.tags = ['jadibot']
+handler.help = ['delsession']
+handler.command = /^(deletesess?ion|eliminarsesion|borrarsesion|delsess?ion|cerrarsesion|delserbot|logout)$/i
+//handler.private = true
+handler.fail = null
 
-handler.tags = ['serbot'];
-handler.help = ['delsession'];
-handler.command = /^(deletesess?ion|eliminarsesion|borrarsesion|delsess?ion|cerrarsesion|delserbot|logout)$/i;
-handler.fail = null;
-
-export default handler;
+export default handler
