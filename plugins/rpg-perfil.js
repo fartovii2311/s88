1a1a0e6b8666dcf0a0c7fb0b4beee159c394f7fb
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2025-02-07 02:09:55 -0500

    Update rpg-perfil.js

--- plugins/rpg-perfil.js
+++ plugins/rpg-perfil.js
@@ -25,7 +25,7 @@ let str = `${lenguajeGB.smsPerfil0()}
 
 *⎔ ${lenguajeGB.smsPerfil5()}*
 • \`\`\`${sn}\`\`\``.trim()
-await conn.sendFile(m.chat, pp, fkontak, 'Lynx.jpg', str, m, false, { mentions: [aa] }) 
+await conn.sendFile(m.chat, pp, rcanal, 'Lynx.jpg', str, m, false, { mentions: [aa] }) 
 }
 handler.command = /^perfil|profile?$/i
 handler.register = true
