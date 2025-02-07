9688cdc5a98fa027be50723618c7d40337c4f4b0
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2025-02-07 10:30:53 -0500

    Update grupo-agregar.js

--- plugins/grupo-agregar.js
+++ plugins/grupo-agregar.js
@@ -6,7 +6,6 @@ else who = m.chat
 let name = await conn.getName(m.sender)        
 let user = global.db.data.users[who]
 let nom = conn.getName(m.sender)
-if (!global.db.data.settings[conn.user.jid].restrict) return conn.reply(m.chat, `🚩 *Este comando está deshabilitado por mi creador*`, m, rcanal) 
 if (!text) await m.reply(`Ingrese el número de la persona que quieres añadir a este grupo.\n\n🚩 Ejemplo:\n*${usedPrefix + command}* 66666666666`)
 if (text.includes('+')) await m.reply(`Ingrese el número todo junto sin el *(+)*`)
 let group = m.chat
