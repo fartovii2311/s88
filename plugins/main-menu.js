34d1eab2e1b25cf9278c1f0322b35efd26d9284e
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2025-02-07 02:52:51 -0500

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -210,7 +210,7 @@ try {
 await conn.sendMessage(m.chat, { image: gataImg.getRandom(), gifPlayback: false, caption: menu, mentions: [m.sender, global.conn.user.jid] }, { quoted: fkontak }) 
 } catch (error) {
 try{
-await conn.sendFile(m.chat, imagen5, 'menu.jpg', menu, rcanal, fkontak, false, { mentions: [m.sender, global.conn.user.jid] })
+await conn.sendFile(m.chat, imagen5, 'menu.jpg', menu, rcanal, false, { mentions: [m.sender, global.conn.user.jid] }, { quoted: fkontak })
 } catch (error) {
 return 
 }}}} 
