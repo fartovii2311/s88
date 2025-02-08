ddd519036fb38f01a4021feafcda4d9d52d74260
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2025-02-08 01:23:45 -0500

    Update _autobio.js

--- plugins/_autobio.js
+++ plugins/_autobio.js
@@ -14,7 +14,7 @@ process.once('message', resolve);
 setTimeout(resolve, 2000)}) * 1000;
 }
 let uptime = clockString(_uptime);
-let bio = `${packname} ⁝⁝ ✅ ${uptime} ⌛ ⁝⁝ ${language == 'es' ? '' : '#status #jadibot #owner 💻'} 💻 By: Team Code Titans`;
+let bio = `${global.packname} ⁝⁝ ✅ ${uptime} ⌛ ⁝⁝  ${lenguajeGB.lenguaje() == 'es' ? '#estado #menu #serbot #grupos #creadora' : '#status #menu #jadibot #groupsgb #owner'} 💻 By: DarkCore` 
 await this.updateProfileStatus(bio).catch(_ => _);
 setting.status = new Date() * 1;
 };
